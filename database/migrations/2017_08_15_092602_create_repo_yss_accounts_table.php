428ca4775f4d3780a852a38f47f5141164349170
DuongNguyen3c13 <wanderer.a4@gmail.com>
2017-08-15 09:42:29 +0000

    add @codingStandardsIgnoreLine

--- database/migrations/2017_08_15_092602_create_repo_yss_accounts_table.php
+++ database/migrations/2017_08_15_092602_create_repo_yss_accounts_table.php
@@ -1,10 +1,11 @@
 <?php
 
+// @codingStandardsIgnoreLine
+
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-// @codingStandardsIgnoreLine
 class CreateRepoYssAccountsTable extends Migration
 {
     /**
--- database/migrations/2017_08_15_092631_create_repo_yss_account_report_table.php
+++ database/migrations/2017_08_15_092631_create_repo_yss_account_report_table.php
@@ -1,10 +1,11 @@
 <?php
 
+// @codingStandardsIgnoreLine
+
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-// @codingStandardsIgnoreLine
 class CreateRepoYssAccountReportTable extends Migration
 {
     /**
