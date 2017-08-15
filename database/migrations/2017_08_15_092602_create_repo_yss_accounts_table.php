82a868cd15aa4102177eed7f2717cf1a6e13926a
DuongNguyen3c13 <wanderer.a4@gmail.com>
2017-08-15 09:58:14 +0000

    update migration files

--- database/migrations/2017_08_15_092602_create_repo_yss_accounts_table.php
+++ database/migrations/2017_08_15_092602_create_repo_yss_accounts_table.php
@@ -1,11 +1,10 @@
 <?php
 
-// @codingStandardsIgnoreLine
-
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
+// @codingStandardsIgnoreLine
 class CreateRepoYssAccountsTable extends Migration
 {
     /**
@@ -17,12 +16,28 @@ class CreateRepoYssAccountsTable extends Migration
     {
         Schema::create('repo_yss_accounts', function (Blueprint $table) {
             $table->increments('id', 11)->index();
-            $table->bigInteger('accountid')->comment('アカウントID')->nullable()->unsigned();
-            $table->string('account_id', 50)->comment('ADgainerシステムのアカウントID')->nullable();
-            $table->string('accountName', 255)->comment('アカウント名')->nullable();
-            $table->string('accountType', 20)->comment('料金の支払い方法 https://github.com/yahoojp-marketing/sponsored-search-api-documents/blob/master/docs/ja/api_reference/data/AccountType.md')->nullable();
-            $table->string('accountStatus', 20)->comment('アカウントの契約状況 https://github.com/yahoojp-marketing/sponsored-search-api-documents/blob/master/docs/ja/api_reference/data/AccountStatus.md')->nullable();
-            $table->string('deliveryStatus', 20)->comment('広告の配信状況 https://github.com/yahoojp-marketing/sponsored-search-api-documents/blob/master/docs/ja/api_reference/data/DeliveryStatus.md')->nullable();
+            $table->bigInteger('accountid')
+                    ->comment('アカウントID')
+                    ->nullable()
+                    ->unsigned();
+            $table->string('account_id', 50)
+                    ->comment('ADgainerシステムのアカウントID')->nullable();
+            $table->string('accountName', 255)
+                    ->comment('アカウント名')->nullable();
+            $table->string('accountType', 20)
+                    ->comment('料金の支払い方法 https://github.com/yahoojp-marketing/'
+                        . 'sponsored-search-api-documents/blob/master/docs/ja/'
+                        . 'api_reference/data/AccountType.md')
+                    ->nullable();
+            $table->string('accountStatus', 20)
+                    ->comment('アカウントの契約状況 https://github.com/yahoojp-marketing/'
+                        .'sponsored-search-api-documents/blob/master/docs/ja'
+                        .'/api_reference/data/AccountStatus.md')->nullable();
+            $table->string('deliveryStatus', 20)
+                    ->comment('広告の配信状況 https://github.com/yahoojp-marketing/'
+                        .'sponsored-search-api-documents/blob/master/docs/ja'
+                        .'
+                        /api_reference/data/DeliveryStatus.md')->nullable();
             $table->timestamp('created_at')->comment('作成日時')->nullable();
             $table->timestamp('updated_at')->comment('更新日時')->nullable();
         });
@@ -38,5 +53,3 @@ class CreateRepoYssAccountsTable extends Migration
         Schema::drop('repo_yss_account');
     }
 }
-
-
--- database/migrations/2017_08_15_092631_create_repo_yss_account_report_table.php
+++ database/migrations/2017_08_15_092631_create_repo_yss_account_report_table.php
@@ -1,11 +1,10 @@
 <?php
 
-// @codingStandardsIgnoreLine
-
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
+// @codingStandardsIgnoreLine
 class CreateRepoYssAccountReportTable extends Migration
 {
     /**
@@ -17,20 +16,32 @@ class CreateRepoYssAccountReportTable extends Migration
     {
         Schema::create('repo_yss_account_report', function (Blueprint $table) {
             $table->increments('id', 11)->index();
-            $table->string('account_id', 50)->comment('ADgainerシステムのアカウントID')->nullable();
-            $table->string('campaign_id', 50)->comment('ADgainerシステムのキャンペーンID destinationURLのクエリパラメータを分解して取得')->nullable();
-            $table->bigInteger('cost')->comment('ADgainerシステムのキャンペーンID destinationURLのクエリパラメータを分解して取得')->nullable()->unsigned();
-            $table->bigInteger('impressions')->comment('インプレッション数')->nullable()->unsigned();
-            $table->bigInteger('clicks')->comment('クリック数')->nullable()->unsigned();
+            $table->string('account_id', 50)
+                    ->comment('ADgainerシステムのアカウントID')->nullable();
+            $table->string('campaign_id', 50)
+                    ->comment('ADgainerシステムのキャンペーンID'
+                    .'destinationURLのクエリパラメータを分解して取得')->nullable();
+            $table->bigInteger('cost')
+                    ->comment('ADgainerシステムのキャンペーンID'
+                    .' destinationURLのクエリパラメータを分解して取得')
+                    ->nullable()->unsigned();
+            $table->bigInteger('impressions')
+                    ->comment('インプレッション数')->nullable()->unsigned();
+            $table->bigInteger('clicks')
+                    ->comment('クリック数')->nullable()->unsigned();
             $table->double('ctr')->comment('クリック率')->nullable();
             $table->double('averageCpc')->comment('平均CPC')->nullable();
             $table->double('averagePosition')->comment('平均掲載順位')->nullable();
-            $table->bigInteger('invalidClicks')->comment('無効なクリック')->nullable()->unsigned();
+            $table->bigInteger('invalidClicks')
+                    ->comment('無効なクリック')->nullable()->unsigned();
             $table->double('invalidClickRate')->comment('無効なクリック率')->nullable();
             $table->double('impressionShare')->comment('インプレッションシェア')->nullable();
-            $table->double('exactMatchImpressionShare')->comment('完全一致のインプレッションシェア')->nullable();
-            $table->double('budgetLostImpressionShare')->comment('インプレッション損失率（予算）')->nullable();
-            $table->double('qualityLostImpressionShare')->comment('インプレッション損失率（掲載順位）')->nullable();
+            $table->double('exactMatchImpressionShare')
+                    ->comment('完全一致のインプレッションシェア')->nullable();
+            $table->double('budgetLostImpressionShare')
+                    ->comment('インプレッション損失率（予算）')->nullable();
+            $table->double('qualityLostImpressionShare')
+                    ->comment('インプレッション損失率（掲載順位）')->nullable();
             $table->text('trackingURL')->comment('トラッキングURL')->nullable();
             $table->double('conversions')->comment('コンバージョン数')->nullable();
             $table->double('convRate')->comment('コンバージョン率')->nullable();
@@ -44,7 +55,9 @@ class CreateRepoYssAccountReportTable extends Migration
             $table->double('valuePerAllConv')->comment('価値/すべてのコンバージョン数')->nullable();
             $table->string('network', 50)->comment('広告掲載方式の指定')->nullable();
             $table->double('device', 50)->comment('デバイス')->nullable();
-            $table->dateTime('day', 50)->comment('レコードの対象日：年（year）、月（monthofYear）、日（day）。左項目を加工してDATETIMEに変換')->nullable();
+            $table->dateTime('day', 50)
+                    ->comment('レコードの対象日：年（year）、月（monthofYear）、日'
+                        .'（day）。左項目を加工してDATETIMEに変換')->nullable();
             $table->string('dayOfWeek', 50)->comment('曜日')->nullable();
             $table->string('quarter', 50)->comment('四半期')->nullable();
             $table->string('month', 50)->comment('毎月')->nullable();
@@ -62,5 +75,3 @@ class CreateRepoYssAccountReportTable extends Migration
         Schema::drop('repo_yss_account_report');
     }
 }
-
-
