57e4ee31f223f5228e1964fab1d8535b54a6459e
Youri Westerman <y.westerman@scuti.asia>
2017-08-17 16:08:39 +0700

    Refactored excel export so it can be tested

--- app/Export/MaatwebsiteExcelExporter.php
+++ app/Export/MaatwebsiteExcelExporter.php
@@ -9,6 +9,10 @@ class MaatwebsiteExcelExporter implements ExcelExporterInterface
 {
     /** @var \App\AbstractReportModel */
     private $model;
+
+    /** @var string */
+    private $fileName;
+
     /**
     * @param AbstractReportModel $model
     */
@@ -17,24 +21,42 @@ class MaatwebsiteExcelExporter implements ExcelExporterInterface
         $this->model = $model;
     }
 
+    /**
+     * @return string
+     */
+    public function getFileName()
+    {
+        return $this->fileName;
+    }
+
+    /**
+     * @return string
+     */
     public function export()
     {
         // get report model fields' data
         $reports = $this->model->get();
+
         // get table name
         $tableName = $this->model->getTable();
-        // remove pagination on view
-        $export = true;
+
         // get fields' names
         $fieldNames = $this->model->getColumnNames();
-        $fileName = date("Y_m_d") . " " . date("h_i") ." ". "$tableName";
-        Excel::create($fileName, function ($excel) use ($reports, $fieldNames, $export, $tableName) {
-            $excel->sheet($tableName, function ($sheet) use ($reports, $fieldNames, $export) {
+
+        $this->fileName = date("Y_m_d h_i ") . "{$tableName}";
+
+        $excelData = Excel::create($this->fileName, function ($excel) use ($reports, $fieldNames, $tableName) {
+            $excel->sheet($tableName, function ($sheet) use ($reports, $fieldNames) {
                 $sheet->loadView('layouts.table_data')
                       ->with('reports', $reports)
                       ->with('fieldNames', $fieldNames)
-                      ->with('export', $export);
+                      ->with('export', true); // remove pagination on view
+
             });
-        })->download('xlsx');
+        })->string('xlsx');
+
+        $this->fileName .= '.xlsx';
+
+        return $excelData;
     }
 }
