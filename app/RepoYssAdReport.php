449d30393c93ea5f85b880d37e12a62bc82024e6
Youri Westerman <y.westerman@scuti.asia>
2017-08-23 16:42:21 +0700

    Added missing comma

--- app/RepoYssAdReport.php
+++ app/RepoYssAdReport.php
@@ -11,7 +11,7 @@ class RepoYssAdReport extends Model
         'account_id',    // Account ID
         'campaign_id',    // Campaign ID of ADgainer system
         'campaignID',    //
-        'adgroupID'
+        'adgroupID',
         'adID',    //
         'campaignName',    //
         'adGroupName',    //
