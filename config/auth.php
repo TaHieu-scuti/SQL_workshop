4b05c3685c3c8e2dec3bc791bee7bb5b447ed74c
Youri Westerman <y.westerman@scuti.asia>
2017-08-31 16:18:12 +0700

    Changed driver value of the user provider configuration for authentication
    Former-commit-id: fb82d0300ca35ffd1cccecd01f6611996cc5c04a

--- config/auth.php
+++ config/auth.php
@@ -66,7 +66,7 @@ return [
 
     'providers' => [
         'users' => [
-            'driver' => 'eloquent',
+            'driver' => 'adgainer',
             'model' => App\User::class,
         ],
 
