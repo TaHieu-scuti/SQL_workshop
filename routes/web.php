774a41f299e0c3c3383605125ca22dbabdb38b1d
Youri Westerman <y.westerman@scuti.asia>
2017-08-31 16:49:39 +0700

    Improved readability of the routes file
    Former-commit-id: b7ed7aa7bf17ec8eeacf501499a4a2fd966abf95

--- routes/web.php
+++ routes/web.php
@@ -8,16 +8,37 @@
 
 Auth::routes();
 
-Route::get('/account_report', 'RepoYssAccountReport\RepoYssAccountReportController@index');
-Route::post('/update-table', 'RepoYssAccountReport\RepoYssAccountReportController@updateTable');
+Route::get(
+    '/account_report',
+    'RepoYssAccountReport\RepoYssAccountReportController@index'
+);
+
+Route::post(
+    '/update-table',
+    'RepoYssAccountReport\RepoYssAccountReportController@updateTable'
+);
+
 Route::get(
     '/account_report/export_excel',
     'RepoYssAccountReport\RepoYssAccountReportController@exportToExcel'
 )->name('export_excel');
+
 Route::get(
     '/account_report/export_csv',
     'RepoYssAccountReport\RepoYssAccountReportController@exportToCsv'
 )->name('export_csv');
-Route::get('/display-graph', 'RepoYssAccountReport\RepoYssAccountReportController@displayDataOnGraph');
-Route::post('/display-graph', 'RepoYssAccountReport\RepoYssAccountReportController@updateGraph');
-Route::post('/account_report/live_search', 'RepoYssAccountReport\RepoYssAccountReportController@liveSearch');
+
+Route::get(
+    '/display-graph',
+    'RepoYssAccountReport\RepoYssAccountReportController@displayDataOnGraph'
+);
+
+Route::post(
+    '/display-graph',
+    'RepoYssAccountReport\RepoYssAccountReportController@updateGraph'
+);
+
+Route::post(
+    '/account_report/live_search',
+    'RepoYssAccountReport\RepoYssAccountReportController@liveSearch'
+);
