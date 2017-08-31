468f5f38a88bdaff9b4ced758213634405f28ae5
Youri Westerman <y.westerman@scuti.asia>
2017-08-31 11:29:49 +0700

    Modified test to use the same DateTime object in all tests
    Former-commit-id: b864c5e0f5935ef8ff322b8989a6e3fac0bbe638

--- tests/Feature/ExcelExportYSSAccountReportTest.php
+++ tests/Feature/ExcelExportYSSAccountReportTest.php
@@ -4,71 +4,66 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 
-use Illuminate\Foundation\Testing\TestResponse;
-
 use DateTime;
 use ZipArchive;
 
 class ExcelExportYSSAccountReportTest extends TestCase
 {
-    /** @var DateTime */
-    private $now;
-
     public function testReturnsStatus200()
     {
-        $this->now = new DateTime;
+        $now = new DateTime;
 
         $response = $this->get('/account_report/export_excel');
 
         $response->assertStatus(200);
 
-        return $response;
+        return ['response' => $response, 'now' => $now];
     }
 
     /**
      * @depends testReturnsStatus200
-     * @param TestResponse $response
+     * @param array $response
      */
-    public function testReturnsCorrectResponseHeaders(TestResponse $response)
+    public function testReturnsCorrectResponseHeaders(array $response)
     {
-        $response->assertHeader(
+        $response['response']->assertHeader(
             'Content-Type',
             'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet; charset=UTF-8'
         );
 
-        $fileName = $this->now->format("Y_m_d h_i ") . 'repo_yss_account_report.xlsx';
-        $response->assertHeader('Content-Disposition', 'attachment; filename="' . $fileName . '"');
-        $response->assertHeader('Expires', 'Mon, 26 Jul 1997 05:00:00 GMT');
+        $fileName = $response['now']->format("Y_m_d h_i ") . 'repo_yss_account_report.xlsx';
+        $response['response']->assertHeader('Content-Disposition', 'attachment; filename="' . $fileName . '"');
+        $response['response']->assertHeader('Expires', 'Mon, 26 Jul 1997 05:00:00 GMT');
 
-        $response->assertHeader('Last-Modified');
+        $response['response']->assertHeader('Last-Modified');
 
-        $lastModifiedDateTime = $this->now->format('D, d M Y H:i:s');
-        $lastModifiedHeader = $response->headers->get('Last-Modified');
+        $lastModifiedDateTime = $response['now']->format('D, d M Y H:i:s');
+        $lastModifiedHeader = $response['response']->headers->get('Last-Modified');
 
         $i = 0;
-        while ($lastModifiedDateTime !== $lastModifiedHeader && $i < 4) {
-            $lastModifiedDateTime = $this->now->modify('+1 second')->format('D, d M Y H:i:s');
+        while ($lastModifiedDateTime !== $lastModifiedHeader && $i < 30) {
+            $lastModifiedDateTime = $response['now']->modify('+1 second')->format('D, d M Y H:i:s');
             $i++;
         }
 
         $this->assertSame($lastModifiedDateTime, $lastModifiedHeader);
 
-        $response->assertHeader('Cache-Control', 'cache, must-revalidate, private');
-        $response->assertHeader('Pragma', 'public');
+        $response['response']->assertHeader('Cache-Control', 'cache, must-revalidate, private');
+        $response['response']->assertHeader('Pragma', 'public');
     }
 
     /**
      * @depends testReturnsStatus200
-     * @param TestResponse $response
+     * @param array $response
      */
-    public function testReturnsCorrectContent(TestResponse $response)
+    public function testReturnsCorrectContent(array $response)
     {
         $resourceZipArchive = new ZipArchive;
         $resourceZipArchive->open(__DIR__ . '/../resources/repo_yss_account_report.xlsx');
         $expectedSheet = $resourceZipArchive->getFromName('xl/worksheets/sheet1.xml');
 
         $fileName = tempnam('/tmp', 'repo_yss_account_report');
-        file_put_contents($fileName, $response->getContent());
+        file_put_contents($fileName, $response['response']->getContent());
 
         $actualZipArchive = new ZipArchive;
         $actualZipArchive->open($fileName);
