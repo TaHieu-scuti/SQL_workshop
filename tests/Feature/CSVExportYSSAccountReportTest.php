a9a4a08e5eeb58e5c5aa5b593ab97b26d9e86cfa
Youri Westerman <y.westerman@scuti.asia>
2017-08-31 11:38:20 +0700

    Using the same DateTime object in all tests
    Former-commit-id: 024c8c865b0e6922dc72c3eadc46120707974d46

--- tests/Feature/CSVExportYSSAccountReportTest.php
+++ tests/Feature/CSVExportYSSAccountReportTest.php
@@ -4,58 +4,57 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 
-use Illuminate\Foundation\Testing\TestResponse;
-
 use DateTime;
 
 class CSVExportYSSAccountReportTest extends TestCase
 {
     public function testReturnsStatus200()
     {
+        $now = new DateTime;
+
         $response = $this->get('/account_report/export_csv');
 
         $response->assertStatus(200);
 
-        return $response;
+        return ['response' => $response, 'now' => $now];
     }
 
     /**
      * @depends testReturnsStatus200
-     * @param TestResponse $response
+     * @param array $response
      */
-    public function testReturnsCorrectResponseHeaders(TestResponse $response)
+    public function testReturnsCorrectResponseHeaders(array $response)
     {
-        $response->assertHeader('Content-Type', 'application/csv; charset=UTF-8');
+        $response['response']->assertHeader('Content-Type', 'application/csv; charset=UTF-8');
 
-        $fileName = date("Y_m_d h_i ") . 'repo_yss_account_report.csv';
-        $response->assertHeader('Content-Disposition', 'attachment; filename="' . $fileName . '"');
-        $response->assertHeader('Expires', 'Mon, 26 Jul 1997 05:00:00 GMT');
+        $fileName = $response['now']->format("Y_m_d h_i ") . 'repo_yss_account_report.csv';
+        $response['response']->assertHeader('Content-Disposition', 'attachment; filename="' . $fileName . '"');
+        $response['response']->assertHeader('Expires', 'Mon, 26 Jul 1997 05:00:00 GMT');
 
-        $response->assertHeader('Last-Modified');
+        $response['response']->assertHeader('Last-Modified');
 
-        $now = new DateTime;
-        $lastModifiedDateTime = $now->format('D, d M Y H:i:s');
-        $lastModifiedHeader = $response->headers->get('Last-Modified');
+        $lastModifiedDateTime = $response['now']->format('D, d M Y H:i:s');
+        $lastModifiedHeader = $response['response']->headers->get('Last-Modified');
 
         $i = 0;
-        while ($lastModifiedDateTime !== $lastModifiedHeader && $i < 4) {
-            $lastModifiedDateTime = $now->modify('+1 second')->format('D, d M Y H:i:s');
+        while ($lastModifiedDateTime !== $lastModifiedHeader && $i < 30) {
+            $lastModifiedDateTime = $response['now']->modify('+1 second')->format('D, d M Y H:i:s');
             $i++;
         }
 
         $this->assertSame($lastModifiedDateTime, $lastModifiedHeader);
 
-        $response->assertHeader('Cache-Control', 'cache, must-revalidate, private');
-        $response->assertHeader('Pragma', 'public');
+        $response['response']->assertHeader('Cache-Control', 'cache, must-revalidate, private');
+        $response['response']->assertHeader('Pragma', 'public');
     }
 
     /**
      * @depends testReturnsStatus200
-     * @param TestResponse $response
+     * @param array $response
      */
-    public function testReturnsCorrectContent(TestResponse $response)
+    public function testReturnsCorrectContent(array $response)
     {
         $expectedContent = file_get_contents(__DIR__ . '/../resources/repo_yss_account_report.csv');
-        $this->assertSame($expectedContent, $response->getContent());
+        $this->assertSame($expectedContent, $response['response']->getContent());
     }
 }
