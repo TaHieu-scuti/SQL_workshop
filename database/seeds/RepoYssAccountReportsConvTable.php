865814ea8fbca5b529271b4683d8d78390ecdec5
tungnx <tungxn1594@gmail.com>
2017-09-27 17:00:16 +0700

    add seeder file for repo yss account report convs
    Former-commit-id: 1addd64bd98772b95c04b855c0fcb8ac6c278b4a

---
+++ database/seeds/RepoYssAccountReportsConvTable.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use DB;
+
+class RepoYssAccountReportsConvTable extends Seeder
+{
+    const OBJECTIVE_OF_CONVERSION_TRACKING = 'Conversion Tracking';
+    const CONVERSION_NAME = 'Conversion Name';
+    const START_DATE = '2017-01-01 00:00:00';
+    const END_DATE = '2018-02-03 00:00:00';
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $yssAccountReports = App\RepoYssAccountReport::query()->get();
+        $startDate = new DateTime(self::START_DATE);
+        $endDate = new DateTime(self::END_DATE);
+        
+        foreach ($yssAccountReports as $yssAccountReport) {
+            $day = 
+            DB::table('repo_yss_campaign_report_convs')->insert([
+                'exeDate' => $endDate,
+                'startDate' => $startDate,
+                'endDate' => $endDate,
+                'account_id' => $yssAccountReport->account_id,
+                'campaign_id' => $yssAccountReport->campaign_id,
+                'trackingURL' => $yssAccountReport->trackingURL,
+                'conversions' => $yssAccountReport->conversions,
+                'convValue' => $yssAccountReport->convValue,
+                'valuePerConv' => $yssAccountReport->valuePerConv,
+                'allConv' => $yssAccountReport->allConv,
+                'allConvValue' => $yssAccountReport->allConvValue,
+                'valuePerAllConv' => $yssAccountReport->valuePerAllConv,
+                'network' => $yssAccountReport->network,
+                'clickType' => $yssAccountReport->clickType,
+                'device' => $yssAccountReport->device,
+                'day' => $yssAccountReport->day,
+                'dayOfWeek' => $yssAccountReport->dayOfWeek,
+                'quarter' => $yssAccountReport->quarter,
+                'month' => $yssAccountReport->month,
+                'week' => $yssAccountReport->week,
+                'objectiveOfConversionTracking' => self::OBJECTIVE_OF_CONVERSION_TRACKING,
+                'conversionName' => self::CONVERSION_NAME,
+            ]);
+        }
+    }
+}
