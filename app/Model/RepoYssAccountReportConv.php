66fcc8f7a93ce5284a4cdcc77728370b7814d704
hieutq <quanghieuit.hubt@gmail.com>
2017-09-27 11:10:21 +0700

    change path model
    Former-commit-id: 8f2f427ded2a6a66dc7ecddac5ce6887b6db6b81

--- app/Model/RepoYssAccountReportConv.php
+++ app/Model/RepoYssAccountReportConv.php
@@ -4,7 +4,7 @@ namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
 
-class RepoYssAccountReportConv extends AbstractReportModel
+class RepoYssAccountReportConv extends \App\AbstractReportModel
 {
     //
 }
