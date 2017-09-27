8ed2188ae0955fa6c4787d811bca9ce426516650
hieutq <quanghieuit.hubt@gmail.com>
2017-09-27 11:09:16 +0700

    change path for model
    Former-commit-id: 2dc87c6d5608b8d9c6ddc3dc316f0ce529f5ca22

--- app/Model/RepoYssCampaignReportConv.php
+++ app/Model/RepoYssCampaignReportConv.php
@@ -4,7 +4,7 @@ namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
 
-class RepoYssCampaignReportConv extends AbstractReportModel
+class RepoYssCampaignReportConv extends \App\AbstractReportModel
 {
     //
 }
--- app/Model/RepoYssCampaignReportCost.php
+++ app/Model/RepoYssCampaignReportCost.php
@@ -4,7 +4,7 @@ namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
 
-class RepoYssCampaignReportCost extends AbstractReportModel
+class RepoYssCampaignReportCost extends \App\AbstractReportModel
 {
     //
 }
