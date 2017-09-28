a0810508360a39f21e5ef4f5c4a98c19a511686d
tungnx <tungxn1594@gmail.com>
2017-09-28 10:33:37 +0700

    change the way of seeding into get content from sql file
    Former-commit-id: a966635387db2b950828285bb79e3769b399c08a

--- database/seeds/RepoYssAccountReportsConvTable.php
+++ database/seeds/RepoYssAccountReportsConvTable.php
@@ -5,10 +5,6 @@ use Illuminate\Support\Facades\DB;
 
 class RepoYssAccountReportsConvTable extends Seeder
 {
-    const OBJECTIVE_OF_CONVERSION_TRACKING = 'Conversion Tracking';
-    const CONVERSION_NAME = 'Conversion Name';
-    const START_DATE = '2017-01-01 00:00:00';
-    const END_DATE = '2018-02-03 00:00:00';
     /**
      * Run the database seeds.
      *
@@ -16,35 +12,8 @@ class RepoYssAccountReportsConvTable extends Seeder
      */
     public function run()
     {
-        $yssAccountReports = App\RepoYssAccountReport::query()->get();
-        $startDate = new DateTime(self::START_DATE);
-        $endDate = new DateTime(self::END_DATE);
-        
-        foreach ($yssAccountReports as $yssAccountReport) {
-            DB::table('repo_yss_account_report_convs')->insert([
-                'exeDate' => $endDate,
-                'startDate' => $startDate,
-                'endDate' => $endDate,
-                'account_id' => $yssAccountReport->account_id,
-                'campaign_id' => $yssAccountReport->campaign_id,
-                'trackingURL' => $yssAccountReport->trackingURL,
-                'conversions' => $yssAccountReport->conversions,
-                'convValue' => $yssAccountReport->convValue,
-                'valuePerConv' => $yssAccountReport->valuePerConv,
-                'allConv' => $yssAccountReport->allConv,
-                'allConvValue' => $yssAccountReport->allConvValue,
-                'valuePerAllConv' => $yssAccountReport->valuePerAllConv,
-                'network' => $yssAccountReport->network,
-                'clickType' => $yssAccountReport->clickType,
-                'device' => $yssAccountReport->device,
-                'day' => $yssAccountReport->day,
-                'dayOfWeek' => $yssAccountReport->dayOfWeek,
-                'quarter' => $yssAccountReport->quarter,
-                'month' => $yssAccountReport->month,
-                'week' => $yssAccountReport->week,
-                'objectiveOfConversionTracking' => self::OBJECTIVE_OF_CONVERSION_TRACKING,
-                'conversionName' => self::CONVERSION_NAME,
-            ]);
-        }
+        DB::unprepared(
+            file_get_contents(__DIR__ . '/../../database/resources/repo_yss_account_report_convs.sql')
+        );
     }
 }
--- database/seeds/RepoYssAccountReportsCostTable.php
+++ database/seeds/RepoYssAccountReportsCostTable.php
@@ -5,10 +5,6 @@ use Illuminate\Support\Facades\DB;
 
 class RepoYssAccountReportsCostTable extends Seeder
 {
-    const OBJECTIVE_OF_CONVERSION_TRACKING = 'Conversion Tracking';
-    const CONVERSION_NAME = 'Conversion Name';
-    const START_DATE = '2017-01-01 00:00:00';
-    const END_DATE = '2018-02-03 00:00:00';
     /**
      * Run the database seeds.
      *
@@ -16,42 +12,8 @@ class RepoYssAccountReportsCostTable extends Seeder
      */
     public function run()
     {
-        $yssAccountReports = App\RepoYssAccountReport::query()->get();
-        $startDate = new DateTime(self::START_DATE);
-        $endDate = new DateTime(self::END_DATE);
-
-        foreach ($yssAccountReports as $yssAccountReport) {
-            DB::table('repo_yss_account_report_costs')->insert([
-                'exeDate' => $endDate,
-                'startDate' => $startDate,
-                'endDate' => $endDate,
-                'account_id' => $yssAccountReport->account_id,
-                'campaign_id' => $yssAccountReport->campaign_id,
-                'cost' => $yssAccountReport->cost,
-                'impressions' => $yssAccountReport->impressions,
-                'clicks' => $yssAccountReport->clicks,
-                'ctr' => $yssAccountReport->ctr,
-                'averageCpc' => $yssAccountReport->averageCpc,
-                'averagePosition' => $yssAccountReport->averagePosition,
-                'impressionShare' => $yssAccountReport->impressionShare,
-                'exactMatchImpressionShare' => $yssAccountReport->exactMatchImpressionShare,
-                'budgetLostImpressionShare' => $yssAccountReport->budgetLostImpressionShare,
-                'qualityLostImpressionShare' => $yssAccountReport->qualityLostImpressionShare,
-                'trackingURL' => $yssAccountReport->trackingURL,
-                'conversions' => $yssAccountReport->conversions,
-                'convRate' => $yssAccountReport->convRate,
-                'convValue' => $yssAccountReport->convValue,
-                'costPerConv' => $yssAccountReport->costPerConv,
-                'valuePerConv' => $yssAccountReport->valuePerConv,
-                'network' => $yssAccountReport->network,
-                'device' => $yssAccountReport->device,
-                'day' => $yssAccountReport->day,
-                'dayOfWeek' => $yssAccountReport->dayOfWeek,
-                'quarter' => $yssAccountReport->quarter,
-                'month' => $yssAccountReport->month,
-                'week' => $yssAccountReport->week,
-                'hourofday' => rand(0,24)
-            ]);
-        }
+        DB::unprepared(
+            file_get_contents(__DIR__ . '/../../database/resources/repo_yss_account_report_costs.sql')
+        );
     }
 }
