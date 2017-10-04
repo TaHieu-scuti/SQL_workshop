e54fef54877e14c96461d44928c64259b7a7a5e9
tungnx <tungxn1594@gmail.com>
2017-10-04 11:09:24 +0700

    adjust init session method
    Former-commit-id: 3cb14d84bf7e076bc96e01986303ff518c30556f

--- app/Http/Controllers/RepoYssCampaignReport/RepoYssCampaignReportController.php
+++ app/Http/Controllers/RepoYssCampaignReport/RepoYssCampaignReportController.php
@@ -10,11 +10,36 @@ use Illuminate\Http\Request;
 
 class RepoYssCampaignReportController extends AbstractReportController
 {
-    const SESSION_KEY_PREFIX = 'campaignReport.';
+    const TIME_PERIOD_TITLE = 'timePeriodTitle';
     const GRAPH_COLUMN_NAME = 'graphColumnName';
     const START_DAY = 'startDay';
     const END_DAY = 'endDay';
+    const STATUS_TITLE = 'statusTitle';
+    const COLUMN_SORT = 'columnSort';
+    const SORT = 'sort';
+    const SUMMARY_REPORT = 'summaryReport';
+    const SESSION_KEY_PREFIX = 'campaignReport.';
+    const SESSION_KEY_FIELD_NAME = self::SESSION_KEY_PREFIX . 'fieldName';
+    const SESSION_KEY_TIME_PERIOD_TITLE = self::SESSION_KEY_PREFIX. self::TIME_PERIOD_TITLE;
+    const SESSION_KEY_STATUS_TITLE = self::SESSION_KEY_PREFIX . self::STATUS_TITLE;
+    const SESSION_KEY_START_DAY = self::SESSION_KEY_PREFIX . self::START_DAY;
+    const SESSION_KEY_END_DAY = self::SESSION_KEY_PREFIX . self::END_DAY;
+    const SESSION_KEY_PAGINATION = self::SESSION_KEY_PREFIX . 'pagination';
+    const SESSION_KEY_GRAPH_COLUMN_NAME = self::SESSION_KEY_PREFIX . self::GRAPH_COLUMN_NAME;
+    const SESSION_KEY_COLUMN_SORT = self::SESSION_KEY_PREFIX . self::COLUMN_SORT;
+    const SESSION_KEY_SORT = self::SESSION_KEY_PREFIX . self::SORT;
+    const SESSION_KEY_SUMMARY_REPORT = self::SESSION_KEY_PREFIX . self::SUMMARY_REPORT;
 
+    const REPORTS = 'reports';
+    const FIELD_NAMES = 'fieldNames';
+    const TOTAL_DATA_ARRAY = 'totalDataArray';
+    const COLUMNS = 'columns';
+    const COLUMNS_FOR_LIVE_SEARCH = 'columnsLiveSearch';
+    const KEY_PAGINATION = 'keyPagination';
+
+    const COLUMNS_FOR_FILTER = 'columnsInModal';
+
+    /** @var \App\Model\RepoYssCampaignReportCost */
     protected $model;
 
     public function __construct(
@@ -38,6 +63,31 @@ class RepoYssCampaignReportController extends AbstractReportController
         return $this->responseFactory->json($errorObject, 500);
     }
 
+    private function initializeSession(array $columns)
+    {
+        $today = new DateTime;
+        $endDay = $today->format('Y-m-d');
+        $startDay = $today->modify('-90 days')->format('Y-m-d');
+        $timePeriodTitle = "Last 90 days";
+        $graphColumnName = 'clicks';
+        $summaryReport = [
+            'clicks',
+            'impressions',
+            'cost',
+            'averageCpc',
+            'averagePosition',
+        ];
+        session([self::SESSION_KEY_FIELD_NAME => $columns]);
+        session([self::SESSION_KEY_TIME_PERIOD_TITLE => $timePeriodTitle]);
+        session([self::SESSION_KEY_START_DAY => $startDay]);
+        session([self::SESSION_KEY_END_DAY => $endDay]);
+        session([self::SESSION_KEY_PAGINATION => 20]);
+        session([self::SESSION_KEY_GRAPH_COLUMN_NAME => $graphColumnName]);
+        session([self::SESSION_KEY_COLUMN_SORT => 'impressions']);
+        session([self::SESSION_KEY_SORT => 'desc']);
+        session([self::SESSION_KEY_SUMMARY_REPORT => $summaryReport]);
+    }
+
     private function getDataForGraph()
     {
         $data = $this->model->newGetDataForGraph($graphColumnName, $startDay, $endDay);
@@ -46,14 +96,46 @@ class RepoYssCampaignReportController extends AbstractReportController
 
     public function index()
     {
-        dd(request()->route()->getPrefix());
-        return view('yssCampaignReport.index');
+        $allColumns = $this->model->getColumnNames();
+        $unpossibleColumnsDisplay = [
+            'exeDate',
+            'startDate',
+            'endDate',
+            'account_id',
+            'campaignID',
+            'campaignName',
+            'campaignDistributionSettings',
+            'campaignDistributionStatus',
+            'campaignStartDate',
+            'campaignEndDate',
+            'trackingURL',
+            'customParameters',
+            'campaignTrackingID',
+            'network',
+            'device',
+            'day',
+            'dayOfWeek',
+            'quarter',
+            'month',
+            'week',
+            'hourofday',
+            'campaignType'
+        ];
+        $availableColumns = $this->model->unsetColumns($allColumns, $unpossibleColumnsDisplay);
+        $modalAndSearchColumnsArray = $availableColumns;
+        array_unshift($availableColumns, 'accountName');
+        if (!session('campaignReport')) {
+            $this->initializeSession($availableColumns);
+        }
+        $prefixRoute = request()->route()->getPrefix();
+        return view('yssCampaignReport.index', [
+            'prefixRoute' => $prefixRoute
+        ]);
     }
 
     public function displayGraph(Request $request)
     {
         $page_prefix = $request->route()->getPrefix();
-        dd($page_prefix);
         $data = $this->getDataForGraph();
     }
 }
