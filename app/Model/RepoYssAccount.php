abc10308e38a47c3a2076a27fbf2039d777ca21b
hieutq <quanghieuit.hubt@gmail.com>
2017-10-23 14:03:22 +0700

    commit file model
    Former-commit-id: 7df8d8d66b9d77b449d52e0eb025546d92ec5bb6
    Former-commit-id: 0cbb1d1acc86399cc1f2aa46e50204275ffeaf05

---
+++ app/Model/RepoYssAccount.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class RepoYssAccount extends Model
+{
+    protected $table = 'repo_yss_accounts';
+    protected $fillable = [
+        'accountid',    // Account ID
+        'account_id',    // Account ID of ADgainer system
+        'accountName',    // Account name
+        'accountType',    // Type of account
+        'accountStatus',    // Contract status of the account
+        'deliveryStatus',    // Delivery status of the ad
+    ];
+}
