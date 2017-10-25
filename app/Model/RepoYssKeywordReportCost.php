f7aa49a6ad723e7959a001f47ba8de166d027f6b
Youri Westerman <y.westerman@scuti.asia>
2017-10-25 13:37:38 +0700

    Removed skeleton for getDataForTable since that function now resides in the abstract model
    Former-commit-id: 4e2815b53447716eda6e38b5bab7f7c78f6029b2
    Former-commit-id: 8f175dcbed4e7238ae8e9aba21124256cd560292

--- app/Model/RepoYssKeywordReportCost.php
+++ app/Model/RepoYssKeywordReportCost.php
@@ -6,31 +6,17 @@ use App\AbstractReportModel;
 
 class RepoYssKeywordReportCost extends AbstractReportModel
 {
+    const FIELDS = [
+        'keywordID',
+        'keyword'
+    ];
+
+    const GROUPED_BY_FIELD_NAME = 'keywordID';
+
     /** @var bool */
     public $timestamps = false;
 
     protected $table = 'repo_yss_keyword_report_cost';
-
-    /**
-     * @param string[] $fieldNames
-     * @param string   $accountStatus
-     * @param string   $startDay
-     * @param string   $endDay
-     * @param int      $pagination
-     * @param string   $columnSort
-     * @param string   $sort
-     * @return string[]
-     */
-    public function getDataForTable(
-        array $fieldNames,
-        $accountStatus,
-        $startDay,
-        $endDay,
-        $pagination,
-        $columnSort,
-        $sort
-    ) {
-    }
 
     /**
      * @param string $column
