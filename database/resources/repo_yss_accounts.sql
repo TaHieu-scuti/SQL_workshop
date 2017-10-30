b7de42896af5250aa45d1f70f61f13e92546370c
Youri Westerman <y.westerman@scuti.asia>
2017-10-30 15:10:59 +0700

    Updated resources for database seeders
    Former-commit-id: 3a032042e4d5da57eb245430c9c7282172d9a0a0
    Former-commit-id: 190e2493b446b9c3fdbd15fef2b0a41622b3f999

--- database/resources/repo_yss_accounts.sql
+++ database/resources/repo_yss_accounts.sql
@@ -1,36 +1,28 @@
--- MySQL dump 10.13  Distrib 5.7.19, for Linux (x86_64)
---
--- Host: localhost    Database: adgainer_db_secures
--- ------------------------------------------------------
--- Server version	5.7.19-0ubuntu0.16.04.1
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
---
--- Dumping data for table `repo_yss_accounts`
---
+/*!40101 SET NAMES utf8mb4 */;
 
-LOCK TABLES `repo_yss_accounts` WRITE;
-/*!40000 ALTER TABLE `repo_yss_accounts` DISABLE KEYS */;
-INSERT INTO `repo_yss_accounts` VALUES (1,650,'1','olbZQGLxK2','lK4OGGZgVu','enabled','enabled','2017-09-19 00:48:15','2017-09-19 00:48:15'),(2,27,'2','n4ZDkajrir','xzDOrYNlRY','enabled','enabled','2017-09-19 00:48:16','2017-09-19 00:48:16'),(3,83,'3','sTaagkkYwn','v6E5q7LLdy','enabled','enabled','2017-09-19 00:48:16','2017-09-19 00:48:16'),(4,794,'4','SAdqHlv98A','j2KwTrtqlL','enabled','enabled','2017-09-19 00:48:17','2017-09-19 00:48:17'),(5,383,'5','DgAVzE2lec','iL9pnY62n9','enabled','enabled','2017-09-19 00:48:17','2017-09-19 00:48:17');
-/*!40000 ALTER TABLE `repo_yss_accounts` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+INSERT INTO `repo_yss_accounts` (`id`, `accountid`, `account_id`, `accountName`, `accountType`, `accountStatus`, `deliveryStatus`, `created_at`, `updated_at`) VALUES
+(1, 11, '1', '4vad6Qr2EF', 'Lr2JgM0XUV', 'enabled', 'enabled', '2017-10-30 08:08:45', '2017-10-30 08:08:45'),
+(2, 12, '1', 'T5BynTbDij', 'h2yBMfoMr3', 'enabled', 'enabled', '2017-10-30 08:08:45', '2017-10-30 08:08:45'),
+(3, 21, '2', 'Pw5E9Ftr9T', '2LL24mDbnC', 'enabled', 'enabled', '2017-10-30 08:08:45', '2017-10-30 08:08:45'),
+(4, 22, '2', 'a62SQCcysA', 'zXLwstTxfi', 'enabled', 'enabled', '2017-10-30 08:08:45', '2017-10-30 08:08:45'),
+(5, 23, '2', 'WVU0RIU7mv', 'wQrRjheoYz', 'enabled', 'enabled', '2017-10-30 08:08:45', '2017-10-30 08:08:45'),
+(6, 24, '2', '389fmtpFUf', 'F0qmoyzcLo', 'enabled', 'enabled', '2017-10-30 08:08:45', '2017-10-30 08:08:45'),
+(7, 31, '3', 'OzydvYZFZF', 'xQnBlQo69y', 'enabled', 'enabled', '2017-10-30 08:08:45', '2017-10-30 08:08:45'),
+(8, 32, '3', '0AqDBb3j4N', 'VHlPzpFS2Y', 'enabled', 'enabled', '2017-10-30 08:08:45', '2017-10-30 08:08:45'),
+(9, 33, '3', 'P3cgYdk81F', 'VTIrxzZC6l', 'enabled', 'enabled', '2017-10-30 08:08:45', '2017-10-30 08:08:45'),
+(10, 34, '3', 'VpD5qE8oY5', 'FB9TfXCiBu', 'enabled', 'enabled', '2017-10-30 08:08:45', '2017-10-30 08:08:45'),
+(11, 35, '3', 'ik3ThGSbqZ', '5lIIwLpH3G', 'enabled', 'enabled', '2017-10-30 08:08:45', '2017-10-30 08:08:45'),
+(12, 41, '4', 'TSKTdlBzkl', 'HhWlUjH6uo', 'enabled', 'enabled', '2017-10-30 08:08:45', '2017-10-30 08:08:45'),
+(13, 51, '5', '5eLC0SF69t', '03sFyEGJcX', 'enabled', 'enabled', '2017-10-30 08:08:45', '2017-10-30 08:08:45'),
+(14, 52, '5', 'fkR3AnXdyy', 'YrdH9QUwRq', 'enabled', 'enabled', '2017-10-30 08:08:45', '2017-10-30 08:08:45'),
+(15, 53, '5', 'ZEsetzDgZ8', 'h1DF60Gz3V', 'enabled', 'enabled', '2017-10-30 08:08:45', '2017-10-30 08:08:45');
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2017-09-19 15:22:48
