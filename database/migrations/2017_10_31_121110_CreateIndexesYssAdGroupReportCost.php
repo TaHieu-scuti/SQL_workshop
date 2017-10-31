be3c9efb201105fb3915c3d3f49a469b37b494de
Youri Westerman <y.westerman@scuti.asia>
2017-10-31 19:17:52 +0700

    Added indexes
    Former-commit-id: 650c518f98692909c17edae9cea42d82ce30579c

---
+++ database/migrations/2017_10_31_121110_CreateIndexesYssAdGroupReportCost.php
@@ -0,0 +1,69 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateIndexesYssAdGroupReportCost extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table(
+            'repo_yss_adgroup_report_cost',
+            function (Blueprint $table) {
+                $table->index('account_id', 'repo_yss_adgroup_report_cost_idx1');
+                $table->index('campaign_id', 'repo_yss_adgroup_report_cost_idx2');
+                $table->index('campaignID', 'repo_yss_adgroup_report_cost_idx3');
+                $table->index('adgroupID', 'repo_yss_adgroup_report_cost_idx4');
+                $table->index('network', 'repo_yss_adgroup_report_cost_idx5');
+                $table->index('device', 'repo_yss_adgroup_report_cost_idx6');
+                $table->index('day', 'repo_yss_adgroup_report_cost_idx7');
+                $table->index('dayOfWeek', 'repo_yss_adgroup_report_cost_idx8');
+                $table->index('quarter', 'repo_yss_adgroup_report_cost_idx9');
+                $table->index('month', 'repo_yss_adgroup_report_cost_idx10');
+                $table->index('week', 'repo_yss_adgroup_report_cost_idx11');
+                $table->index('hourofday', 'repo_yss_adgroup_report_cost_idx12');
+                $table->index('exeDate', 'repo_yss_adgroup_report_cost_idx13');
+                $table->index('startDate', 'repo_yss_adgroup_report_cost_idx14');
+                $table->index('endDate', 'repo_yss_adgroup_report_cost_idx15');
+                $table->index('accountid', 'repo_yss_adgroup_report_cost_idx16');
+
+            }
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table(
+            'repo_yss_adgroup_report_cost',
+            function (Blueprint $table) {
+                $table->dropIndex('repo_yss_adgroup_report_cost_idx1');
+                $table->dropIndex('repo_yss_adgroup_report_cost_idx2');
+                $table->dropIndex('repo_yss_adgroup_report_cost_idx3');
+                $table->dropIndex('repo_yss_adgroup_report_cost_idx4');
+                $table->dropIndex('repo_yss_adgroup_report_cost_idx5');
+                $table->dropIndex('repo_yss_adgroup_report_cost_idx6');
+                $table->dropIndex('repo_yss_adgroup_report_cost_idx7');
+                $table->dropIndex('repo_yss_adgroup_report_cost_idx8');
+                $table->dropIndex('repo_yss_adgroup_report_cost_idx9');
+                $table->dropIndex('repo_yss_adgroup_report_cost_idx10');
+                $table->dropIndex('repo_yss_adgroup_report_cost_idx11');
+                $table->dropIndex('repo_yss_adgroup_report_cost_idx12');
+                $table->dropIndex('repo_yss_adgroup_report_cost_idx13');
+                $table->dropIndex('repo_yss_adgroup_report_cost_idx14');
+                $table->dropIndex('repo_yss_adgroup_report_cost_idx15');
+                $table->dropIndex('repo_yss_adgroup_report_cost_idx16');
+            }
+        );
+    }
+}
