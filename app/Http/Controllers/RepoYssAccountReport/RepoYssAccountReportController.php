483e6698aa4093d8d4106feb7a2c6ff98c136c12
hieutq-scuti <hieutq@scuti.asia>
2017-11-06 13:32:14 +0700

    fix some code wrong

--- app/Http/Controllers/RepoYssAccountReport/RepoYssAccountReportController.php
+++ app/Http/Controllers/RepoYssAccountReport/RepoYssAccountReportController.php
@@ -89,6 +89,7 @@ class RepoYssAccountReportController extends AbstractReportController
         if (session(self::SESSION_KEY_FIELD_NAME)) {
             if (session(self::SESSION_KEY_FIELD_NAME)[0] === 'device'
                 || session(self::SESSION_KEY_FIELD_NAME)[0] === 'hourofday'
+                || session(self::SESSION_KEY_FIELD_NAME)[0] === 'dayOfWeek'
             ) {
                 $fieldNames = session(self::SESSION_KEY_FIELD_NAME);
                 $fieldNames[0] = self::SESSION_KEY_GROUPED_BY_FIELD;
