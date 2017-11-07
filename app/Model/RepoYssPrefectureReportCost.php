b07dcf68d8cfc680118814e5a2d6a985ac334a48
tungnx <tungxn1594@gmail.com>
2017-11-07 12:12:36 +0700

    adjust prefecture model

--- app/Model/RepoYssPrefectureReportCost.php
+++ app/Model/RepoYssPrefectureReportCost.php
@@ -3,7 +3,6 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
-use App\AbstractReportModel;
 
 class RepoYssPrefectureReportCost extends Model
 {
@@ -11,40 +10,4 @@ class RepoYssPrefectureReportCost extends Model
 
     /** @var bool */
     public $timestamps = false;
-
-    /**
-     * @param string[] $fieldNames
-     * @param string   $accountStatus
-     * @param string   $startDay
-     * @param string   $endDay
-     * @param int      $pagination
-     * @param string   $columnSort
-     * @param string   $sort
-     * @return string[]
-     */
-    public function getDataForTable(
-        array $fieldNames,
-        $accountStatus,
-        $startDay,
-        $endDay,
-        $pagination,
-        $columnSort,
-        $sort
-    ) {
-    }
-
-    /**
-     * @param string $column
-     * @param string $accountStatus
-     * @param string $startDay
-     * @param string $endDay
-     * @return \Illuminate\Support\Collection
-     */
-    public function getDataForGraph(
-        $column,
-        $accountStatus,
-        $startDay,
-        $endDay
-    ) {
-    }
 }
