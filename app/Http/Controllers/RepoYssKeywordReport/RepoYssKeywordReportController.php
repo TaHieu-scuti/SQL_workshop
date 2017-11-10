b3a2e110558bdafc268553efe5283e1d3be86d65
Youri Westerman <y.westerman@scuti.asia>
2017-11-10 13:59:40 +0700

    Using prefecture report table when prefecture report data is requested

--- app/Http/Controllers/RepoYssKeywordReport/RepoYssKeywordReportController.php
+++ app/Http/Controllers/RepoYssKeywordReport/RepoYssKeywordReportController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\RepoYssKeywordReport;
 use App\Http\Controllers\AbstractReportController;
 use App\Model\RepoYssKeywordReportCost;
 
+use App\Model\RepoYssPrefectureReportCost;
 use Illuminate\Contracts\Routing\ResponseFactory;
 use Illuminate\Http\Request;
 
@@ -102,6 +103,11 @@ class RepoYssKeywordReportController extends AbstractReportController
     public function updateTable(Request $request)
     {
         $this->updateSessionData($request);
+
+        if ($request->specificItem === 'prefecture') {
+            $this->model = new RepoYssPrefectureReportCost;
+        }
+
         $reports = $this->getDataForTable();
         $totalDataArray = $this->getCalculatedData();
         $summaryReportData = $this->getCalculatedSummaryReport();
