fe29b4d159242a8582775a963f85f4f0bccab2a0
= <nguyenchitien195@gmail.com>
2017-11-13 18:24:13 +0700

    create migrations for adw adgroup and adw geo

---
+++ app/Model/RepoAdwGeoReportCost.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class RepoAdwGeoReportCost extends Model
+{
+    protected $table = 'repo_adw_geo_report_cost';
+    public $timestamps = false;
+}
