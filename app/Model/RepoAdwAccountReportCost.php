14cb15f13ea67206dbbd4cc070f6fe50e79a5a47
hieutq-scuti <admin@Admins-MacBook-Pro.local>
2017-11-13 17:09:46 +0700

    add some code for model

--- app/Model/RepoAdwAccountReportCost.php
+++ app/Model/RepoAdwAccountReportCost.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class RepoAdwAccountReportCost extends Model
 {
-    //
+    protected $table = 'repo_adw_account_report_cost';
+
+    /** @var bool */
+    public $timestamps = false;
 }
--- app/Model/RepoAdwAdReportCost.php
+++ app/Model/RepoAdwAdReportCost.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class RepoAdwAdReportCost extends Model
 {
-    //
+    protected $table = "repo_adw_ad_report_cost";
+
+    /** @var bool */
+    public $timestamps = false;
 }
