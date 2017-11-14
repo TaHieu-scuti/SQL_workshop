0212cddc65bd0100fb3448cefa06e50c71f6e407
hieutq-scuti <admin@Admins-MacBook-Pro.local>
2017-11-14 13:48:19 +0700

    add file seed for adw_account_report

---
+++ database/seeds/RepoAdwAccountReportCostTable.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class RepoAdwAccountReportCostTable extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $command = 'mysql -h'
+            . Config::get('database.connections.mysql.host')
+            . ' -u'
+            . Config::get('database.connections.mysql.username')
+            . ' -p'
+            . Config::get('database.connections.mysql.password')
+            . ' '
+            . Config::get('database.connections.mysql.database')
+            . ' < '
+            . __DIR__ . '/../../database/resources/repo_adw_account_report_cost.sql';
+
+        exec($command);
+    }
+}
