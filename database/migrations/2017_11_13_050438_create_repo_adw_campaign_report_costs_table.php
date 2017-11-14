8743b0e3627fee1b52a132ab96633a417ac20def
Duong Nguyen <wanderer.a4@gmail.com>
2017-11-14 08:45:57 +0000

    2017_11_13_050438_create_repo_adw_campaign_report_costs_table.php edited online with Bitbucket

--- database/migrations/2017_11_13_050438_create_repo_adw_campaign_report_costs_table.php
+++ database/migrations/2017_11_13_050438_create_repo_adw_campaign_report_costs_table.php
@@ -303,7 +303,7 @@ class CreateRepoAdwCampaignReportCostsTable extends Migration
                     ->nullable()
                     ->comment('年はyyyyの形式です。')
                     ->index('repo_adw_campaign_report_cost17');
-            $table->bigInteger('accountId')
+            $table->bigInteger('accountid')
                     ->comment('media Id');
             $table->
         });
