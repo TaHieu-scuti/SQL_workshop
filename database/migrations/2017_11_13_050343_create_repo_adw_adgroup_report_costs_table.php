63057a1c69d5e5f646324e89d2528a7b479a4bc4
Youri Westerman <y.westerman@scuti.asia>
2017-11-15 17:07:04 +0700

    Fixed style errors

--- database/migrations/2017_11_13_050343_create_repo_adw_adgroup_report_costs_table.php
+++ database/migrations/2017_11_13_050343_create_repo_adw_adgroup_report_costs_table.php
@@ -25,14 +25,23 @@ class CreateRepoAdwAdgroupReportCostsTable extends Migration
                 $table->string('campaign_id', 50)->comment('ADgainerシステムのキャンペーンID。destinationURLのクエリパラメータを分解して取得');
                 $table->string('currency')->nullable()->comment('顧客アカウントの通貨。');
                 $table->text('account')->nullable()->comment('カスタマーアカウントのわかりやすい名前。');
-                $table->string('timeZone')->nullable()->comment('顧客アカウント用に選択されたタイムゾーンの名前。たとえば、「（GMT-05：00）東部時間」などです。このフィールドには、タイムゾーンの夏時間の現在の状態は反映されません。');
+                $table->string('timeZone')
+                    ->nullable()
+                    ->comment(
+                        '顧客アカウント用に選択されたタイムゾーンの名前。たとえば、「（GMT-05：00）東部時間」などです。'
+                        . 'このフィールドには、タイムゾーンの夏時間の現在の状態は反映されません。'
+                    );
                 $table->double('activeViewAvgCPM')->nullable()->comment('視認可能インプレッションの平均費用（ActiveViewImpressions）。');
                 $table->double('activeViewViewableCTR')->nullable()->comment('広告が表示された後にユーザーが広告をクリックした頻度');
                 $table->bigInteger('activeViewViewableImpressions')->nullable()->comment('ディスプレイネットワークサイトで広告が表示される頻度');
-                $table->double('activeViewMeasurableImprImpr')->nullable()->comment('アクティブビューで計測されたインプレッション数と配信済みインプレッション数の比。');
+                $table->double('activeViewMeasurableImprImpr')
+                    ->nullable()
+                    ->comment('アクティブビューで計測されたインプレッション数と配信済みインプレッション数の比。');
                 $table->double('activeViewMeasurableCost')->nullable()->comment('Active Viewで測定可能なインプレッションの費用。');
                 $table->bigInteger('activeViewMeasurableImpr')->nullable()->comment('広告が表示されているプレースメントに広告が表示された回数。');
-                $table->double('activeViewViewableImprMeasurableImpr')->nullable()->comment('広告がアクティブビュー対応サイトに表示された時間（測定可能なインプレッション数）と表示可能（表示可能なインプレッション数）の割合。');
+                $table->double('activeViewViewableImprMeasurableImpr')
+                    ->nullable()
+                    ->comment('広告がアクティブビュー対応サイトに表示された時間（測定可能なインプレッション数）と表示可能（表示可能なインプレッション数）の割合。');
                 $table->double('desktopBidAdj')->nullable()->comment('広告グループレベルでデスクトップの入札単価調整が上書きされます。');
                 $table->bigInteger('adGroupID')->nullable()->comment('広告グループのID。');
                 $table->double('mobileBidAdj')->nullable()->comment('広告グループレベルでモバイルの入札単価調整が上書きされます。');
@@ -46,31 +55,83 @@ class CreateRepoAdwAdgroupReportCostsTable extends Migration
                 $table->double('avgCPC')->nullable()->comment('すべてのクリックの総コストを、受け取った総クリック数で割った値。');
                 $table->double('avgCPM')->nullable()->comment('平均インプレッション単価（CPM）。');
                 $table->double('avgPosition')->nullable()->comment('他の広告主様との相対的な広告の掲載順位');
-                $table->bigInteger('baseAdGroupID')->nullable()->comment('試用広告グループの基本広告グループのID。通常の広告グループの場合、これはAdGroupIdと同じです。');
-                $table->bigInteger('baseCampaignID')->nullable()->comment('試用キャンペーンの基本キャンペーンのID。通常のキャンペーンの場合、これはCampaignIdと同じです。');
+                $table->bigInteger('baseAdGroupID')
+                    ->nullable()
+                    ->comment('試用広告グループの基本広告グループのID。通常の広告グループの場合、これはAdGroupIdと同じです。');
+                $table->bigInteger('baseCampaignID')
+                    ->nullable()
+                    ->comment('試用キャンペーンの基本キャンペーンのID。通常のキャンペーンの場合、これはCampaignIdと同じです。');
                 $table->bigInteger('bidStrategyID')->nullable()->comment('BiddingStrategyConfigurationのIDです。');
                 $table->text('bidStrategyName')->nullable()->comment('BiddingStrategyConfigurationの名前。');
-                $table->string('biddingStrategySource', 50)->nullable()->comment('入札戦略が関連付けられている場所（キャンペーン、広告グループ、広告グループの条件など）を示します。');
+                $table->string('biddingStrategySource', 50)
+                    ->nullable()
+                    ->comment('入札戦略が関連付けられている場所（キャンペーン、広告グループ、広告グループの条件など）を示します。');
                 $table->string('bidStrategyType', 50)->nullable()->comment('BiddingStrategyConfigurationのタイプ。');
                 $table->string('conversionOptimizerBidType', 50)->nullable()->comment('入札タイプ。');
                 $table->bigInteger('campaignID')->nullable()->comment('キャンペーンのID。');
                 $table->text('campaign')->nullable()->comment('キャンペーンの名前。');
                 $table->string('campaignState', 50)->nullable()->comment('キャンペーンのステータス。');
                 $table->bigInteger('clicks')->nullable()->comment('クリック数。');
-                $table->string('clickType', 50)->nullable()->comment('[インプレッション数]フィールドには、そのクリックタイプで広告が配信された頻度が反映されます。 広告は複数のクリックタイプで表示できるため、インプレッション数は2倍になり、合計が正確でない可能性があります。');
-                $table->string('contentNetworkBidDimension', 50)->nullable()->comment('広告グループでディスプレイネットワークの絶対的な入札単価に使用する条件のタイプ。');
-                $table->double('convRate')->nullable()->comment('コンバージョン数をコンバージョンにトラッキングできる合計クリック数で割ったものです。 "x.xx％"として返されるパーセンテージ。');
-                $table->double('conversions')->nullable()->comment('最適化を選択したすべてのコンバージョンアクションのコンバージョン数。このフィールドは、小数点の区切り文字として小数点以下2桁の小数点以下桁区切り記号（例：1000000.00）のドット（"."）でフォーマットされます。');
-                $table->double('totalConvValue')->nullable()->comment('すべてのコンバージョンのコンバージョン値の合計。このフィールドは、小数点の区切り文字として小数点以下2桁の小数点以下桁区切り記号（例：1000000.00）のドット（"."）でフォーマットされます。');
+                $table->string('clickType', 50)
+                    ->nullable()
+                    ->comment(
+                        '[インプレッション数]フィールドには、そのクリックタイプで広告が配信された頻度が反映されます。 広告は複数のクリックタイプで表示できるため、'
+                            . 'インプレッション数は2倍になり、合計が正確でない可能性があります。'
+                    );
+                $table->string('contentNetworkBidDimension', 50)
+                    ->nullable()
+                    ->comment('広告グループでディスプレイネットワークの絶対的な入札単価に使用する条件のタイプ。');
+                $table->double('convRate')
+                    ->nullable()
+                    ->comment('コンバージョン数をコンバージョンにトラッキングできる合計クリック数で割ったものです。 "x.xx％"として返されるパーセンテージ。');
+                $table->double('conversions')
+                    ->nullable()
+                    ->comment(
+                        '最適化を選択したすべてのコンバージョンアクションのコンバージョン数。このフィールドは、'
+                        . '小数点の区切り文字として小数点以下2桁の小数点以下桁区切り記号（例：1000000.00）のドット（"."）でフォーマットされます。'
+                    );
+                $table->double('totalConvValue')
+                    ->nullable()
+                    ->comment(
+                        'すべてのコンバージョンのコンバージョン値の合計。このフィールドは、小数点の区切り文字として小数点以下2桁の小数点以下桁区切り記号'
+                        . '（例：1000000.00）のドット（"."）でフォーマットされます。'
+                    );
                 $table->double('cost')->nullable()->comment('この期間のクリック単価（CPC）とインプレッション単価（CPM）の合計。');
                 $table->double('costConv')->nullable()->comment('コンバージョントラッキングクリック数に起因する費用をコンバージョン数で割った値');
-                $table->double('costConvCurrentModel')->nullable()->comment('現在選択しているアトリビューションモデルで、過去の「CostPerConversion」データがどのように表示されるかを示します。このフィールドは、小数点の区切り文字として小数点以下2桁の小数点以下桁区切り記号（例：1000000.00）のドット（"."）でフォーマットされます。');
-                $table->double('defaultMaxCPC')->nullable()->comment('クリック単価制。値は、a）小額の金額、b）AdWordsが自動的に選択された入札戦略で入札単価を設定する場合は「自動：x」または「自動」、c）クリック単価が適用されない場合は「 - 」のいずれかです行に');
+                $table->double('costConvCurrentModel')
+                    ->nullable()
+                    ->comment(
+                        '現在選択しているアトリビューションモデルで、過去の「CostPerConversion」データがどのように表示されるかを示します。このフィールドは、'
+                        . '小数点の区切り文字として小数点以下2桁の小数点以下桁区切り記号（例：1000000.00）のドット（"."）でフォーマットされます。'
+                    );
+                $table->double('defaultMaxCPC')
+                    ->nullable()
+                    ->comment(
+                        'クリック単価制。値は、a）小額の金額、b）AdWordsが自動的に選択された入札戦略で入札単価を設定する場合は「自動：x」または「自動」、'
+                        . 'c）クリック単価が適用されない場合は「 - 」のいずれかです行に'
+                    );
                 $table->double('maxCPM')->nullable()->comment('CPM（1,000インプレッションあたりの単価）の単価');
-                $table->double('maxCPV')->nullable()->comment('視聴単価制の入札単価値は、a）小額の金額、b）AdWordsが自動的に選択した入札戦略で入札単価を設定している場合は「自動：x」または「自動」、またはc）入札単価が適用されない場合は「 - 」のいずれかです行に');
-                $table->double('ctr')->nullable()->comment('広告がクリックされた回数（クリック数）を広告が表示された回数（インプレッション数）で割ったものです。 "x.xx％"として返されるパーセンテージ。');
-                $table->double('conversionsCurrentModel')->nullable()->comment('現在選択しているアトリビューションモデルでの過去の「コンバージョン」データの表示方法を示します。このフィールドは、小数点の区切り文字として小数点以下2桁の小数点以下桁区切り記号（例：1000000.00）のドット（"."）でフォーマットされます。');
-                $table->double('convValueCurrentModel')->nullable()->comment('現在選択しているアトリビューションモデルで、過去の「ConversionValue」データがどのように表示されるかを示します。このフィールドは、小数点の区切り文字として小数点以下2桁の小数点以下桁区切り記号（例：1000000.00）のドット（"."）でフォーマットされます。');
+                $table->double('maxCPV')
+                    ->nullable()
+                    ->comment(
+                        '視聴単価制の入札単価値は、a）小額の金額、b）AdWordsが自動的に選択した入札戦略で入札単価を設定している場合は「自動：x」または「自動」、'
+                           . 'またはc）入札単価が適用されない場合は「 - 」のいずれかです行に'
+                    );
+                $table->double('ctr')
+                    ->nullable()
+                    ->comment('広告がクリックされた回数（クリック数）を広告が表示された回数（インプレッション数）で割ったものです。 "x.xx％"として返されるパーセンテージ。');
+                $table->double('conversionsCurrentModel')
+                    ->nullable()
+                    ->comment(
+                        '現在選択しているアトリビューションモデルでの過去の「コンバージョン」データの表示方法を示します。このフィールドは、'
+                        . '小数点の区切り文字として小数点以下2桁の小数点以下桁区切り記号（例：1000000.00）のドット（"."）でフォーマットされます。'
+                    );
+                $table->double('convValueCurrentModel')
+                    ->nullable()
+                    ->comment(
+                        '現在選択しているアトリビューションモデルで、過去の「ConversionValue」データがどのように表示されるかを示します。このフィールドは、'
+                        . '小数点の区切り文字として小数点以下2桁の小数点以下桁区切り記号（例：1000000.00）のドット（"."）でフォーマットされます。'
+                    );
                 $table->text('clientName')->nullable()->comment('カスタマーのわかりやすい名前。');
                 $table->date('day')->nullable()->comment('日付はyyyy-MM-ddの形式になります。');
                 $table->string('dayOfWeek', 50)->nullable()->comment('曜日の名前です（例：「月曜日」）。');
@@ -84,24 +145,47 @@ class CreateRepoAdwAdgroupReportCostsTable extends Migration
                 $table->bigInteger('gmailSaves')->nullable()->comment('Gmail広告をメッセージとして受信トレイに保存した回数。');
                 $table->bigInteger('gmailClicksToWebsite')->nullable()->comment('Gmail広告の展開状態でのリンク先ページへのクリック数。');
                 $table->integer('hourOfDay')->nullable()->comment('1日の時間は0と23の間の数値です。');
-                $table->bigInteger('impressions')->nullable()->comment('Googleネットワークの検索結果ページやウェブサイトに広告が表示された回数をカウントします。');
-                $table->double('interactionRate')->nullable()->comment('広告が表示された後にユーザーがどのくらい頻繁に広告を操作するか。これはインタラクションの数を広告の表示回数で割ったものです。 "x.xx％"として返されるパーセンテージ。');
-                $table->bigInteger('interactions')->nullable()->comment('相互作用の数 インタラクションとは、テキストやショッピング広告のクリック、動画広告の表示など、広告フォーマットに関連する主要なユーザーアクションです。');
-                $table->text('interactionTypes')->nullable()->comment('Interactions、InteractionRate、およびAverageCostの各列に反映される相互作用のタイプ。');
+                $table->bigInteger('impressions')
+                    ->nullable()
+                    ->comment('Googleネットワークの検索結果ページやウェブサイトに広告が表示された回数をカウントします。');
+                $table->double('interactionRate')
+                    ->nullable()
+                    ->comment('広告が表示された後にユーザーがどのくらい頻繁に広告を操作するか。これはインタラクションの数を広告の表示回数で割ったものです。 "x.xx％"として返されるパーセンテージ。');
+                $table->bigInteger('interactions')
+                    ->nullable()
+                    ->comment('相互作用の数 インタラクションとは、テキストやショッピング広告のクリック、動画広告の表示など、広告フォーマットに関連する主要なユーザーアクションです。');
+                $table->text('interactionTypes')
+                    ->nullable()
+                    ->comment('Interactions、InteractionRate、およびAverageCostの各列に反映される相互作用のタイプ。');
                 $table->text('labelIDs')->nullable()->comment('この行のメインオブジェクトのラベルIDのリスト。リスト要素はJSONリスト形式で返されます。');
                 $table->text('labels')->nullable()->comment('この行のメインオブジェクトのラベル名のリスト。リスト要素はJSONリスト形式で返されます。');
                 $table->date('month')->nullable()->comment('月の最初の日。yyyy-MM-ddの形式です。');
                 $table->string('monthOfYear', 50)->nullable()->comment('月の名前です（例：「12月」）。');
                 $table->bigInteger('phoneImpressions')->nullable()->comment('オフライン電話のインプレッション数。');
                 $table->bigInteger('phoneCalls')->nullable()->comment('オフライン通話の数。');
-                $table->double('ptr')->nullable()->comment('受信した通話の数（NumOfflineInteractions）を、電話番号が表示された回数で割った数（NumOfflineImpressions）。');
-                $table->date('quarter')->nullable()->comment('四半期の最初の日は、yyyy-MM-ddの形式です。四半期の暦年を使用します。たとえば、2014年第2四半期は2014-04-01に開始します。');
+                $table->double('ptr')
+                    ->nullable()
+                    ->comment('受信した通話の数（NumOfflineInteractions）を、電話番号が表示された回数で割った数（NumOfflineImpressions）。');
+                $table->date('quarter')
+                    ->nullable()
+                    ->comment('四半期の最初の日は、yyyy-MM-ddの形式です。四半期の暦年を使用します。たとえば、2014年第2四半期は2014-04-01に開始します。');
                 $table->double('targetCPA')->nullable()->comment('ターゲットコンバージョン単価の入札戦略で設定された平均コンバージョン単価ターゲット。');
-                $table->string('targetCPASource', 50)->nullable()->comment('目標コンバージョン単価が設定されたレベル。 これは広告グループレベルでのみ適用されます。');
+                $table->string('targetCPASource', 50)
+                    ->nullable()
+                    ->comment('目標コンバージョン単価が設定されたレベル。 これは広告グループレベルでのみ適用されます。');
                 $table->text('trackingTemplate')->nullable()->comment('この行のメインオブジェクトのトラッキングテンプレート。');
-                $table->text('customParameter')->nullable()->comment('この行のメインオブジェクトのカスタムURLパラメータ。 CustomParameters要素はJSONマップ形式で返されます。');
-                $table->double('valueConv')->nullable()->comment('コンバージョン数の合計をコンバージョン数で割った値。 このフィールドは、小数点の区切り文字としてドット（"."）でフォーマットされます（例：1000000.00）。');
-                $table->double('valueConvCurrentModel')->nullable()->comment('現在選択しているアトリビューションモデルで、過去の「ValuePerConversion」データがどのように表示されるかを示します。このフィールドは、このフィールドは、小数点の区切り文字としてドット（"."）でフォーマットされます（例：1000000.00）。');
+                $table->text('customParameter')
+                    ->nullable()
+                    ->comment('この行のメインオブジェクトのカスタムURLパラメータ。 CustomParameters要素はJSONマップ形式で返されます。');
+                $table->double('valueConv')
+                    ->nullable()
+                    ->comment('コンバージョン数の合計をコンバージョン数で割った値。 このフィールドは、小数点の区切り文字としてドット（"."）でフォーマットされます（例：1000000.00）。');
+                $table->double('valueConvCurrentModel')
+                    ->nullable()
+                    ->comment(
+                        '現在選択しているアトリビューションモデルで、過去の「ValuePerConversion」データがどのように表示されるかを示します。このフィールドは、このフィールドは、'
+                        . '小数点の区切り文字としてドット（"."）でフォーマットされます（例：1000000.00）。'
+                    );
                 $table->date('week')->nullable()->comment('yyyy-MM-ddの形式の月曜日の日付。');
                 $table->integer('year')->nullable()->comment('年はyyyyの形式です。');
                 $table->bigInteger('accountid')->comment('media id');
