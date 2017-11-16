81c8079901221b4bfcf18261ecb3fc193464c629
DuongNguyen3c13 <wanderer.a4@gmail.com>
2017-11-16 05:27:38 +0000

    updated adw campaign table structure

---
+++ database/migrations/2017_11_16_051752_update_adw_campaign_cost_table_structure.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateAdwCampaignCostTableStructure extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table(
+            'repo_adw_campaign_report_cost',
+            function (Blueprint $table) {
+                $table->string('timeZone', 50)
+                    ->nullable()
+                    ->change();
+            }
+        )
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
