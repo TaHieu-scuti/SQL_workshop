10b4da6a9f31c9123b56b7cdf259b7009822a822
tungnx <tungxn1594@gmail.com>
2017-11-21 13:53:29 +0700

    add timezone data for account report cost table

---
+++ database/seeds/AddingTimezoneDataForAccountReport.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Model\RepoYssAccountReportCost;
+
+class AddingTimezoneDataForAccountReport extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $accounts = RepoYssAccountReportCost::select('id')->get();
+        foreach ($accounts as $account) {
+            $account->hourofday = rand(0,23);
+            $account->saveOrFail();
+        }
+    }
+}
