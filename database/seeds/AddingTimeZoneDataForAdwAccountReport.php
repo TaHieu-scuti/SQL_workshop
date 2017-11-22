1c4e885bdaf6024c7ec83acc859f55b10912a8f4
tungnx <tungxn1594@gmail.com>
2017-11-22 17:00:57 +0700

    fix phpcs

--- database/seeds/AddingTimeZoneDataForAdwAccountReport.php
+++ database/seeds/AddingTimeZoneDataForAdwAccountReport.php
@@ -14,7 +14,7 @@ class AddingTimeZoneDataForAdwAccountReport extends Seeder
     public function run()
     {
         $adwAccounts = RepoAdwAccountReportCost::select('id')->get();
-        foreach($adwAccounts as $adwAccount) {
+        foreach ($adwAccounts as $adwAccount) {
             $adwAccount->hourOfDay = rand(0, 23);
             $adwAccount->saveOrFail();
         }
