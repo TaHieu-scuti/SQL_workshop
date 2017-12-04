4161aa796f7895dc2cd49ae6a4281da74a8e2a06
= <nguyenchitien195@gmail.com>
2017-12-04 17:01:58 +0700

    add more const higher layer for adw and ydn model

--- app/Model/RepoYdnAdReport.php
+++ app/Model/RepoYdnAdReport.php
@@ -9,6 +9,23 @@ class RepoYdnAdReport extends AbstractReportModel
 {
     const GROUPED_BY_FIELD_NAME = 'adName';
     const PAGE_ID = 'adID';
+    const ALL_HIGHER_LAYERS =
+    [
+        [
+            'columnName' => 'campaignName',
+            'tableJoin' => 'repo_ydn_report_cost',
+            'columnId' => 'campaignID',
+            'aliasId' => 'campaignID',
+            'aliasName' => 'campaignName'
+        ],
+        [
+            'columnName' => 'adgroupName',
+            'tableJoin' => 'repo_ydn_report_cost',
+            'columnId' => 'adgroupID',
+            'aliasId' => 'adgroupID',
+            'aliasName' => 'adgroupName',
+        ]
+    ];
 
     protected $table = 'repo_ydn_reports';
     public $timestamps = false;
