605875181058cc55046f424c9dfd8b5ca997f825
tungnx <tungxn1594@gmail.com>
2017-12-04 10:31:37 +0700

    prefecture for adgroup report

--- app/Http/Controllers/RepoYssAdgroupReport/RepoYssAdgroupReportController.php
+++ app/Http/Controllers/RepoYssAdgroupReport/RepoYssAdgroupReportController.php
@@ -95,7 +95,7 @@ class RepoYssAdgroupReportController extends AbstractReportController
             $this->updateGroupByFieldWhenSessionEngineChange($defaultColumns);
         }
         if (session(self::SESSION_KEY_GROUPED_BY_FIELD) === self::PREFECTURE) {
-            $this->model = new RepoYssPrefectureReportCost;
+            $this->updateModelForPrefecture();
         }
         $this->checkoutSessionFieldName();
         $dataReports = $this->getDataForTable();
@@ -171,12 +171,12 @@ class RepoYssAdgroupReportController extends AbstractReportController
         $this->updateSessionData($request);
 
         if (session(self::SESSION_KEY_GROUPED_BY_FIELD) === self::PREFECTURE) {
-            $this->model = new RepoYssPrefectureReportCost;
+            $this->updateModelForPrefecture();
         }
 
         if ($request->specificItem === self::PREFECTURE) {
             session()->put([self::SESSION_KEY_GROUPED_BY_FIELD => self::PREFECTURE]);
-            $this->model = new RepoYssPrefectureReportCost;
+            $this->updateModelForPrefecture();
         }
 
         $reports = $this->getDataForTable();
