0b60e5a8ad5e747e8234cfe9a1ab32ca958f38b0
tungnx <tungxn1594@gmail.com>
2017-12-05 10:34:22 +0700

    fix missing path of RepoYssPrefectureCost Model

--- app/Http/Controllers/AbstractReportController.php
+++ app/Http/Controllers/AbstractReportController.php
@@ -8,6 +8,7 @@ use App\Export\Spout\SpoutExcelExporter;
 use Illuminate\Http\Request;
 use App\Model\RepoAdwGeoReportCost;
 use App\Model\RepoYdnPrefecture;
+use App\Model\RepoYssPrefectureReportCost;
 
 use Illuminate\Contracts\Routing\ResponseFactory;
 
