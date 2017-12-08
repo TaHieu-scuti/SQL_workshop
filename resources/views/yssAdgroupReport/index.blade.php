2e911cd4c66779883a359b8ac23a4a9bc908e3c1
unknown <Dbinh>
2017-12-08 15:37:11 +0700

    Change the session invocation

--- resources/views/yssAdgroupReport/index.blade.php
+++ resources/views/yssAdgroupReport/index.blade.php
@@ -1,3 +1,6 @@
+<?php
+use App\Http\Controllers\RepoYssAdgroupReport\RepoYssAdgroupReportController;
+?>
 @extends('layouts.master')
 
 @section('title')
@@ -11,10 +14,10 @@
                 @lang('language.campaign')
             </a>
         </li>
-        @if (session('adgroupReport.groupedByField') === 'device'
-            || session('adgroupReport.groupedByField') === 'hourofday'
-            || session('adgroupReport.groupedByField') === 'dayOfWeek'
-            || session('adgroupReport.groupedByField') === 'prefecture'
+        @if (session(RepoYssAdgroupReportController::SESSION_KEY_GROUPED_BY_FIELD) === 'device'
+            || session(RepoYssAdgroupReportController::SESSION_KEY_GROUPED_BY_FIELD) === 'hourofday'
+            || session(RepoYssAdgroupReportController::SESSION_KEY_GROUPED_BY_FIELD) === 'dayOfWeek'
+            || session(RepoYssAdgroupReportController::SESSION_KEY_GROUPED_BY_FIELD) === 'prefecture'
         )
             <li class="panel-body normal-report">
                 <a href="javascript:void(0)">
@@ -55,28 +58,28 @@
         <li class="panel-body separator">
         </li>
         <li class="panel-body specific-filter-item
-            @if (session('adgroupReport.groupedByField') === 'prefecture') active @endif"
+            @if (session(RepoYssAdgroupReportController::SESSION_KEY_GROUPED_BY_FIELD) === 'prefecture') active @endif"
             data-value="prefecture">
             <a href="javascript:void(0)">
                 @lang('language.PREFECTURES')
             </a>
         </li>
         <li class="panel-body specific-filter-item 
-            @if (session('adgroupReport.groupedByField') === 'hourofday') active @endif" 
+            @if (session(RepoYssAdgroupReportController::SESSION_KEY_GROUPED_BY_FIELD) === 'hourofday') active @endif" 
             data-value="hourofday">
             <a href="javascript:void(0)">
                 @lang('language.BY_TIME_ZONE')
             </a>
         </li>
         <li class="panel-body specific-filter-item 
-            @if (session('adgroupReport.groupedByField') === 'dayOfWeek') active @endif" 
+            @if (session(RepoYssAdgroupReportController::SESSION_KEY_GROUPED_BY_FIELD) === 'dayOfWeek') active @endif" 
             data-value="dayOfWeek">
             <a href="javascript:void(0)">
                 @lang('language.BY_DAYS_OF_THE_WEEK')
             </a>
         </li>
         <li class="panel-body specific-filter-item 
-            @if (session('adgroupReport.groupedByField') === 'device') active @endif" 
+            @if (session(RepoYssAdgroupReportController::SESSION_KEY_GROUPED_BY_FIELD) === 'device') active @endif" 
             data-value="device">
             <a href="javascript:void(0)">
                 @lang('language.DEVICES')
--- resources/views/yssCampaignReport/index.blade.php
+++ resources/views/yssCampaignReport/index.blade.php
@@ -1,3 +1,6 @@
+<?php
+use App\Http\Controllers\RepoYssCampaignReport\RepoYssCampaignReportController;
+?>
 @extends('layouts.master')
 
 @section('title')
@@ -17,10 +20,10 @@
 
 @section('filter-list')
     <ul class="panel">
-        @if (session('campaignReport.groupedByField') === 'device'
-            || session('campaignReport.groupedByField') === 'hourofday'
-            || session('campaignReport.groupedByField') === 'dayOfWeek'
-            || session('campaignReport.groupedByField') === 'prefecture'
+        @if (session(RepoYssCampaignReportController::SESSION_KEY_GROUPED_BY_FIELD) === 'device'
+            || session(RepoYssCampaignReportController::SESSION_KEY_GROUPED_BY_FIELD) === 'hourofday'
+            || session(RepoYssCampaignReportController::SESSION_KEY_GROUPED_BY_FIELD) === 'dayOfWeek'
+            || session(RepoYssCampaignReportController::SESSION_KEY_GROUPED_BY_FIELD) === 'prefecture'
         )    
             <li class="panel-body normal-report">
                 <a href="javascript:void(0)">
@@ -66,28 +69,28 @@
         <li class="panel-body separator">
         </li>
         <li class="panel-body specific-filter-item
-            @if (session('campaignReport.groupedByField') === 'prefecture') active @endif"
+            @if (session(RepoYssCampaignReportController::SESSION_KEY_GROUPED_BY_FIELD) === 'prefecture') active @endif"
             data-value="prefecture">
             <a href="javascript:void(0)">
                 @lang('language.PREFECTURES')
             </a>
         </li>
         <li class="panel-body specific-filter-item 
-            @if (session('campaignReport.groupedByField') === 'hourofday') active @endif" 
+            @if (session(RepoYssCampaignReportController::SESSION_KEY_GROUPED_BY_FIELD) === 'hourofday') active @endif" 
             data-value="hourofday">
             <a href="javascript:void(0)">
                 @lang('language.BY_TIME_ZONE')
             </a>
         </li>
         <li class="panel-body specific-filter-item 
-            @if (session('campaignReport.groupedByField') === 'dayOfWeek') active @endif" 
+            @if (session(RepoYssCampaignReportController::SESSION_KEY_GROUPED_BY_FIELD) === 'dayOfWeek') active @endif" 
             data-value="dayOfWeek">
             <a href="javascript:void(0)">
                 @lang('language.BY_DAYS_OF_THE_WEEK')
             </a>
         </li>
         <li class="panel-body specific-filter-item 
-            @if (session('campaignReport.groupedByField') === 'device') active @endif" 
+            @if (session(RepoYssCampaignReportController::SESSION_KEY_GROUPED_BY_FIELD) === 'device') active @endif" 
             data-value="device">
             <a href="javascript:void(0)">
                 @lang('language.DEVICES')
