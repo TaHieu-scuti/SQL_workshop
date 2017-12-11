22e9686bc7a57edf0a990d10603da9b5593a1a75
tungnx <tungxn1594@gmail.com>
2017-12-11 14:41:03 +0700

    fix phpcs

--- database/migrations/2017_12_11_031807_create_repo_adw_display_keyword_report_cost.php
+++ database/migrations/2017_12_11_031807_create_repo_adw_display_keyword_report_cost.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
+// @codingStandardsIgnoreLine
 class CreateRepoAdwDisplayKeywordReportCost extends Migration
 {
     /**
@@ -45,7 +46,9 @@ class CreateRepoAdwDisplayKeywordReportCost extends Migration
                 $table->string('adGroupState', 50)->comment('広告グループのステータス。');
                 $table->string('network', 50)->comment('第1レベルのネットワークタイプ。');
                 $table->string('networkWithSearchPartners', 50)->comment('第2レベルのネットワークタイプ（検索パートナーを含む）。');
-                $table->double('allConvRate')->comment('AllConversionsをコンバージョントラッキングできる合計クリック数で割ったものです。これは、広告のクリックがコンバージョンにつながった頻度です。');
+                $table->double('allConvRate')->comment(
+                    'AllConversionsをコンバージョントラッキングできる合計クリック数で割ったものです。これは、広告のクリックがコンバージョンにつながった頻度です。'
+                );
                 $table->double('allConv')->comment('AdWordsが推進するコンバージョン数の最善の見積もり。ウェブサイト、クロスデバイス、電話通話のコンバージョンが含まれます。');
                 $table->double('allConvValue')->comment('推定されたものを含む、すべてのコンバージョンの合計値。');
                 $table->double('avgCost')->comment('インタラクションごとに支払う平均金額。この金額は、広告の合計費用を合計インタラクション数で割ったものです。');
@@ -66,24 +69,44 @@ class CreateRepoAdwDisplayKeywordReportCost extends Migration
                 $table->double('cost')->comment('この期間のクリック単価（CPC）とインプレッション単価（CPM）の合計。');
                 $table->double('costAllConv')->comment('総費用をすべてのコンバージョンで割った値。');
                 $table->double('costConv')->comment('コンバージョントラッキングクリック数に起因する費用をコンバージョン数で割った値');
-                $table->double('maxCPC')->comment('クリック単価制。値は、a）小額の金額、b）AdWordsが自動的に選択された入札戦略で入札単価を設定する場合は「自動：x」または「自動」、c）クリック単価が適用されない場合は「 - 」のいずれかです');
+                $table->double('maxCPC')->comment(
+                    'クリック単価制。値は、a）小額の金額、b）AdWordsが自動的に選択された入札戦略で入札単価を設定する場合は
+                    「自動：x」または「自動」、c）クリック単価が適用されない場合は「 - 」のいずれかです'
+                );
                 $table->string('maxCPCSource', 50)->comment('CPC入札のソース。');
                 $table->double('maxCPM')->comment('CPM（1,000インプレッションあたりの単価）の単価');
                 $table->string('maxCPMSource', 50)->comment('CPM入札のソース。');
-                $table->double('maxCPV')->comment('視聴単価制の入札単価値は、a）小額の金額、b）AdWordsが自動的に選択した入札戦略で入札単価を設定している場合は「自動：x」または「自動」、またはc）入札単価が適用されない場合は「 - 」のいずれかです');
+                $table->double('maxCPV')->comment(
+                    '視聴単価制の入札単価値は、a）小額の金額、b）AdWordsが自動的に選択した入札戦略で入札単価を設定している場合は
+                    「自動：x」または「自動」、またはc）入札単価が適用されない場合は「 - 」のいずれかです'
+                );
                 $table->string('maxCPVSource', 50)->comment('視聴単価の入札価格です。');
-                $table->text('keyword')->comment('Criterionの記述的な文字列。レポートの条件タイプのフォーマットの詳細については、条件プレフィックス(URL:https://developers.google.com/adwords/api/docs/guides/reporting#criteria_prefixes)のセクションをご覧ください。');
+                $table->text('keyword')->comment(
+                    'Criterionの記述的な文字列。レポートの条件タイプのフォーマットの詳細については、条件プレフィックス
+                    (URL:https://developers.google.com/adwords/api/docs/guides/reporting#criteria_prefixes)
+                    のセクションをご覧ください。'
+                );
                 $table->text('destinationURL')->comment('広告を表示した条件のリンク先URL。');
-                $table->double('crossDeviceConv')->comment('顧客が1つの端末でAdWords広告をクリックしてから別の端末やブラウザで変換した後のコンバージョンデバイス間のコンバージョンは既にAllConversions列に含まれています。');
+                $table->double('crossDeviceConv')->comment(
+                    '顧客が1つの端末でAdWords広告をクリックしてから別の端末やブラウザで変換した後のコンバージョンデバイス間のコンバージョンは既に
+                    AllConversions列に含まれています。'
+                );
                 $table->double('ctr')->comment('広告がクリックされた回数（クリック数）を広告が表示された回数（インプレッション数）で割ったものです。');
                 $table->text('clientName')->comment('カスタマーのわかりやすい名前。');
                 $table->date('day')->comment('日付はyyyy-MM-ddの形式になります。');
                 $table->string('dayOfWeek', 50)->comment('曜日の名前です（例：「月曜日」）。');
                 $table->string('device', 50)->comment('インプレッションが表示されたデバイスの種類。');
                 $table->double('engagementRate')->comment('広告が表示された後、ユーザーが広告にどのくらいの頻度で関与するか。 広告の表示回数を広告の表示回数で割ったものです。');
-                $table->bigInteger('engagements')->comment('約束の数。 視聴者がライトボックス広告を展開するとエンゲージメントが発生します。 また、今後、他の広告タイプがエンゲージメント指標をサポートする場合もあります。');
+                $table->bigInteger('engagements')->comment(
+                    '約束の数。 視聴者がライトボックス広告を展開するとエンゲージメントが発生します。
+                    また、今後、他の広告タイプがエンゲージメント指標をサポートする場合もあります。'
+                );
                 $table->bigInteger('customerID')->comment('顧客ID。');
-                $table->text('appFinalURL')->comment('この行のメインオブジェクトの最終的なアプリURLのリスト。リストのエントリは、a）「android-app：」（Androidアプリの場合）またはb）「os-app：」（iOSアプリの場合）のいずれかで始まります。 AppUrlList要素はJSONリスト形式で返されます。');
+                $table->text('appFinalURL')->comment(
+                    'この行のメインオブジェクトの最終的なアプリURLのリスト。リストのエントリは、a）
+                    「android-app：」（Androidアプリの場合）またはb）「os-app：」
+                    （iOSアプリの場合）のいずれかで始まります。 AppUrlList要素はJSONリスト形式で返されます。'
+                );
                 $table->text('mobileFinalURL')->comment('この行のメインオブジェクトの最終的なモバイルURLのリスト。 UrlList要素はJSONリスト形式で返されます。');
                 $table->text('finalURL')->comment('この行の主要オブジェクトの最終的なURLのリスト。 UrlList要素はJSONリスト形式で返されます。');
                 $table->bigInteger('gmailForwards')->comment('広告が誰かにメッセージとして転送された回数。');
@@ -91,26 +114,52 @@ class CreateRepoAdwDisplayKeywordReportCost extends Migration
                 $table->bigInteger('gmailClicksToWebsite')->comment('Gmail広告の展開状態でのリンク先ページへのクリック数。');
                 $table->bigInteger('keywordID')->comment('この行の主オブジェクトのID。');
                 $table->bigInteger('impressions')->comment('Googleネットワークの検索結果ページやウェブサイトに広告が表示された回数をカウントします。');
-                $table->double('interactionRate')->comment('広告が表示された後にユーザーがどのくらい頻繁に広告を操作するか。これはインタラクションの数を広告の表示回数で割ったものです。');
-                $table->bigInteger('interactions')->comment('相互作用の数インタラクションとは、テキストやショッピング広告のクリック、動画広告の表示など、広告フォーマットに関連する主要なユーザーアクショ');
-                $table->string('interactionTypes', 50)->comment('Interactions、InteractionRate、およびAverageCost列に反映される相互作用のタイプ。');
+                $table->double('interactionRate')->comment(
+                    '広告が表示された後にユーザーがどのくらい頻繁に広告を操作するか。
+                    これはインタラクションの数を広告の表示回数で割ったものです。'
+                );
+                $table->bigInteger('interactions')->comment(
+                    '相互作用の数インタラクションとは、テキストやショッピング広告のクリック、
+                    動画広告の表示など、広告フォーマットに関連する主要なユーザーアクショ'
+                );
+                $table->string('interactionTypes', 50)->comment(
+                    'Interactions、InteractionRate、およびAverageCost列に反映される相互作用のタイプ。'
+                );
                 $table->boolean('isNegative')->comment('この行の基準が否定（除外）基準であるかどうかを示します。');
-                $table->boolean('isRestricting')->comment('trueの値は、基準タイプが入札単価とターゲティング制限に使用されていることを示します。 falseの値は、基準タイプが入札にのみ使用されることを示します。これは、基準の対応するAdGroup.TargetingSettingDetailのtargetAllと反対の値になります。たとえば、criterionTypeGroup = PLACEMENTのTargetingSettingDetailにtargetAll = trueが設定されている場合、IsRestrictフィールドは配置基準に対してfalseになります。');
+                $table->boolean('isRestricting')->comment(
+                    'trueの値は、基準タイプが入札単価とターゲティング制限に使用されていることを示します。 falseの値は、基準タイプが入札にのみ使用されることを示します。
+                    これは、基準の対応するAdGroup.TargetingSettingDetailのtargetAllと反対の値になります。
+                    たとえば、criterionTypeGroup = PLACEMENTのTargetingSettingDetailにtargetAll = trueが設定されている場合、
+                    IsRestrictフィールドは配置基準に対してfalseになります。'
+                );
                 $table->date('month')->comment('月の最初の日。yyyy-MM-ddの形式です。');
                 $table->string('monthOfYear', 50)->comment('月の名前です（例：「12月」）。');
-                $table->date('quarter')->comment('四半期の最初の日は、yyyy-MM-ddの形式です。四半期の暦年を使用します。たとえば、2014年第2四半期は2014-04-01に開始します。');
-                $table->string('keywordState', 50)->comment('この行のメインオブジェクトのステータス。たとえば、キャンペーンの掲載結果レポートでは、これが各行のキャンペーンのステータスになります。広告グループの掲載結果レポートでは、これは各行の広告グループのステータスになります。');
+                $table->date('quarter')->comment(
+                    '四半期の最初の日は、yyyy-MM-ddの形式です。四半期の暦年を使用します。
+                    たとえば、2014年第2四半期は2014-04-01に開始します。'
+                );
+                $table->string('keywordState', 50)->comment(
+                    'この行のメインオブジェクトのステータス。たとえば、キャンペーンの掲載結果レポートでは、これが各行のキャンペーンのステータスになります。
+                    広告グループの掲載結果レポートでは、これは各行の広告グループのステータスになります。'
+                );
                 $table->text('trackingTemplate')->comment('この行のメインオブジェクトのトラッキングテンプレート。');
-                $table->text('customParameter')->comment('この行のメインオブジェクトのカスタムURLパラメータ。 CustomParameters要素はJSONマップ形式で返されます。');
+                $table->text('customParameter')->comment(
+                    'この行のメインオブジェクトのカスタムURLパラメータ。 CustomParameters要素はJSONマップ形式で返されます。'
+                );
                 $table->double('valueAllConv')->comment('すべてのコンバージョンの平均値です。');
                 $table->double('valueConv')->comment('コンバージョン数の合計をコンバージョン数で割った値。');
                 $table->double('videoPlayedTo100')->comment('視聴者があなたのすべての動画を視聴したインプレッションの割合。');
                 $table->double('videoPlayedTo25')->comment('視聴者が動画の25％を視聴したインプレッションの割合。');
                 $table->double('videoPlayedTo50')->comment('視聴者が動画の50％を視聴したインプレッションの割合。');
                 $table->double('videoPlayedTo75')->comment('視聴者が動画の75％を視聴したインプレッションの割合。');
-                $table->double('viewRate')->comment('TrueView動画広告の表示回数を、TrueViewインディスプレイ広告のサムネイル表示回数を含むインプレッション数で割ったものです。');
+                $table->double('viewRate')->comment(
+                    'TrueView動画広告の表示回数を、TrueViewインディスプレイ広告のサムネイル表示回数を含むインプレッション数で割ったものです。'
+                );
                 $table->bigInteger('views')->comment('動画広告が表示された回数。');
-                $table->bigInteger('viewThroughConv')->comment('ビュースルーコンバージョンの合計数。これは、ディスプレイネットワーク広告が表示された後、後で他の広告とやり取り（クリックなど）せずにサイトのコンバージョンを達成した場合に発生します。');
+                $table->bigInteger('viewThroughConv')->comment(
+                    'ビュースルーコンバージョンの合計数。これは、ディスプレイネットワーク広告が表示された後、後で他の広告とやり取り
+                    （クリックなど）せずにサイトのコンバージョンを達成した場合に発生します。'
+                );
                 $table->date('week')->comment('yyyy-MM-ddの形式の月曜日の日付。');
                 $table->bigInteger('year')->comment('年はyyyyの形式です。');
                 $table->bigInteger('accountid')->comment('media id');
