d3414e2756216e33c1b55beeba9db06e7ecf0f22
tungnx <tungxn1594@gmail.com>
2017-12-11 13:49:00 +0700

    add RepoAdwDisplayKeywordReportCost model

--- app/Model/RepoAdwDisplayKeywordReportCost.php
+++ app/Model/RepoAdwDisplayKeywordReportCost.php
@@ -3,8 +3,9 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use App\AbstractReportModel;
 
-class RepoAdwDisplayKeywordReportCost extends Model
+class RepoAdwDisplayKeywordReportCost extends AbstractReportModel
 {
     protected $table = "repo_adw_display_keyword_report_cost";
 
