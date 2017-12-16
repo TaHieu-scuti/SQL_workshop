c53a29b7bb7758f862c318ae86d2430c84aaf42c
Youri Westerman <y.westerman@scuti.asia>
2017-12-16 10:03:54 +0700

    Corrected fieldnames, removed campaign_id condition and replaced time_of_call join condition

--- database/design/queries/yss-adgroup-report-call-tracking.sql
+++ database/design/queries/yss-adgroup-report-call-tracking.sql
@@ -1,14 +1,13 @@
 SELECT
 	`repo_yss_adgroup_report_cost`.`account_id`,
-	`repo_yss_adgroup_report_cost`.`campaign_id`,
 	`repo_yss_adgroup_report_cost`.`accountid`,
 	`repo_yss_adgroup_report_cost`.`campaignID`,
-	`repo_yss_adgroup_report_cost`.`adGroupId`,
+	`repo_yss_adgroup_report_cost`.`adgroupID`,
 	SUM(`repo_yss_adgroup_report_cost`.`impressions`) AS impressions,
 	SUM(`repo_yss_adgroup_report_cost`.`clicks`) AS clicks,
 	SUM(`repo_yss_adgroup_report_cost`.`cost`) AS cost,
 	AVG(`repo_yss_adgroup_report_cost`.`ctr`) AS ctr,
-	AVG(`repo_yss_adgroup_report_cost`.`averageCPC`) AS avgCPC,
+	AVG(`repo_yss_adgroup_report_cost`.`averageCpc`) AS avgCPC,
 	COUNT(`phone_time_use`.`id`) AS call_tracking,
 	SUM(`repo_yss_adgroup_report_cost`.`conversions`) AS webcv,
 	SUM(`repo_yss_adgroup_report_cost`.`conversions`) + COUNT(`phone_time_use`.`id`) AS cv,
@@ -27,61 +26,61 @@ FROM
 					(
 						`campaigns`.`camp_custom1` = 'adgroupid'
 					AND
-						`phone_time_use`.`custom1` = `repo_yss_adgroup_report_cost`.`adGroupId`
+						`phone_time_use`.`custom1` = `repo_yss_adgroup_report_cost`.`adgroupID`
 					)
 				OR
 					(
 						`campaigns`.`camp_custom2` = 'adgroupid'
 					AND
-						`phone_time_use`.`custom2` = `repo_yss_adgroup_report_cost`.`adGroupId`
+						`phone_time_use`.`custom2` = `repo_yss_adgroup_report_cost`.`adgroupID`
 					)
 				OR
 					(
 						`campaigns`.`camp_custom3` = 'adgroupid'
 					AND
-						`phone_time_use`.`custom3` = `repo_yss_adgroup_report_cost`.`adGroupId`
+						`phone_time_use`.`custom3` = `repo_yss_adgroup_report_cost`.`adgroupID`
 					)
 				OR
 					(
 						`campaigns`.`camp_custom4` = 'adgroupid'
 					AND
-						`phone_time_use`.`custom4` = `repo_yss_adgroup_report_cost`.`adGroupId`
+						`phone_time_use`.`custom4` = `repo_yss_adgroup_report_cost`.`adgroupID`
 					)
 				OR
 					(
 						`campaigns`.`camp_custom5` = 'adgroupid'
 					AND
-						`phone_time_use`.`custom5` = `repo_yss_adgroup_report_cost`.`adGroupId`
+						`phone_time_use`.`custom5` = `repo_yss_adgroup_report_cost`.`adgroupID`
 					)
 				OR
 					(
 						`campaigns`.`camp_custom6` = 'adgroupid'
 					AND
-						`phone_time_use`.`custom6` = `repo_yss_adgroup_report_cost`.`adGroupId`
+						`phone_time_use`.`custom6` = `repo_yss_adgroup_report_cost`.`adgroupID`
 					)
 				OR
 					(
 						`campaigns`.`camp_custom7` = 'adgroupid'
 					AND
-						`phone_time_use`.`custom7` = `repo_yss_adgroup_report_cost`.`adGroupId`
+						`phone_time_use`.`custom7` = `repo_yss_adgroup_report_cost`.`adgroupID`
 					)
 				OR
 					(
 						`campaigns`.`camp_custom8` = 'adgroupid'
 					AND
-						`phone_time_use`.`custom8` = `repo_yss_adgroup_report_cost`.`adGroupId`
+						`phone_time_use`.`custom8` = `repo_yss_adgroup_report_cost`.`adgroupID`
 					)
 				OR
 					(
 						`campaigns`.`camp_custom9` = 'adgroupid'
 					AND
-						`phone_time_use`.`custom9` = `repo_yss_adgroup_report_cost`.`adGroupId`
+						`phone_time_use`.`custom9` = `repo_yss_adgroup_report_cost`.`adgroupID`
 					)
 				OR
 					(
 						`campaigns`.`camp_custom10` = 'adgroupid'
 					AND
-						`phone_time_use`.`custom10` = `repo_yss_adgroup_report_cost`.`adGroupId`
+						`phone_time_use`.`custom10` = `repo_yss_adgroup_report_cost`.`adgroupID`
 					)
 				)
 			AND
@@ -91,9 +90,7 @@ FROM
 			AND
 				`phone_time_use`.`utm_campaign` = `repo_yss_adgroup_report_cost`.`campaignID`
 			AND
-				`phone_time_use`.`time_of_call` >= '2017-01-01'
-			AND
-				`phone_time_use`.`time_of_call` <= '2017-12-01'
+				STR_TO_DATE(`phone_time_use`.`time_of_call`, '%Y-%m-%d') = `repo_yss_adgroup_report_cost`.`day`
 			AND
 				`phone_time_use`.`source` = 'yss'
 			AND
@@ -101,8 +98,6 @@ FROM
 		)
 WHERE
 	`repo_yss_adgroup_report_cost`.`account_id` = 1
-AND
-	`repo_yss_adgroup_report_cost`.`campaign_id` = 11
 AND
 	`repo_yss_adgroup_report_cost`.`accountid` = 11
 AND
@@ -113,7 +108,6 @@ AND
 	`repo_yss_adgroup_report_cost`.`day` <= '2017-12-01'
 GROUP BY
 	`repo_yss_adgroup_report_cost`.`account_id`,
-	`repo_yss_adgroup_report_cost`.`campaign_id`,
 	`repo_yss_adgroup_report_cost`.`accountid`,
 	`repo_yss_adgroup_report_cost`.`campaignID`,
-	`repo_yss_adgroup_report_cost`.`adGroupId`
+	`repo_yss_adgroup_report_cost`.`adgroupID`
