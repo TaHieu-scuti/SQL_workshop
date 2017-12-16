5106f17e58cc66ed5199697c3e0f0be7dca53635
Youri Westerman <y.westerman@scuti.asia>
2017-12-16 10:07:48 +0700

    Added join conditions for account_id and campaign_id, corrected field name and removed condition on campaign_id

--- database/design/queries/yss-campaign-report-call-tracking.sql
+++ database/design/queries/yss-campaign-report-call-tracking.sql
@@ -1,13 +1,12 @@
 SELECT
 	`repo_yss_campaign_report_cost`.`account_id`,
-	`repo_yss_campaign_report_cost`.`campaign_id`,
 	`repo_yss_campaign_report_cost`.`accountid`,
 	`repo_yss_campaign_report_cost`.`campaignID`,
 	SUM(`repo_yss_campaign_report_cost`.`impressions`) AS impressions,
 	SUM(`repo_yss_campaign_report_cost`.`clicks`) AS clicks,
 	SUM(`repo_yss_campaign_report_cost`.`cost`) AS cost,
 	AVG(`repo_yss_campaign_report_cost`.`ctr`) AS ctr,
-	AVG(`repo_yss_campaign_report_cost`.`averageCPC`) AS avgCPC,
+	AVG(`repo_yss_campaign_report_cost`.`averageCpc`) AS avgCPC,
 	COUNT(`phone_time_use`.`id`) AS call_tracking,
 	SUM(`repo_yss_campaign_report_cost`.`conversions`) AS webcv,
 	SUM(`repo_yss_campaign_report_cost`.`conversions`) + COUNT(`phone_time_use`.`id`) AS cv,
@@ -18,6 +17,10 @@ FROM
 	`repo_yss_campaign_report_cost`
 		LEFT JOIN (`campaigns`, `phone_time_use`)
 		ON (
+				`phone_time_use`.`account_id` = `repo_yss_campaign_report_cost`.`account_id`
+			AND
+				`phone_time_use`.`campaign_id` = `repo_yss_campaign_report_cost`.`campaign_id`
+      AND
 				`phone_time_use`.`utm_campaign` = `repo_yss_campaign_report_cost`.`campaignID`
 			AND
 				`phone_time_use`.`time_of_call` >= '2017-01-01'
@@ -31,8 +34,6 @@ FROM
 WHERE
 	`repo_yss_campaign_report_cost`.`account_id` = 1
 AND
-	`repo_yss_campaign_report_cost`.`campaign_id` = 11
-AND
 	`repo_yss_campaign_report_cost`.`accountid` = 11
 AND
 	`repo_yss_campaign_report_cost`.`day` >= '2017-01-01'
@@ -40,6 +41,5 @@ AND
 	`repo_yss_campaign_report_cost`.`day` <= '2017-12-01'
 GROUP BY
 	`repo_yss_campaign_report_cost`.`account_id`,
-	`repo_yss_campaign_report_cost`.`campaign_id`,
 	`repo_yss_campaign_report_cost`.`accountid`,
 	`repo_yss_campaign_report_cost`.`campaignID`
