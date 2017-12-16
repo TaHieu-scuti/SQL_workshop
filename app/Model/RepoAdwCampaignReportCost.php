9f4f38b873552032d1b2030357ceb0ee63326d41
Youri Westerman <y.westerman@scuti.asia>
2017-12-16 16:31:58 +0700

    Adding join condition

--- app/Model/RepoAdwCampaignReportCost.php
+++ app/Model/RepoAdwCampaignReportCost.php
@@ -2,9 +2,12 @@
 
 namespace App\Model;
 
-use Auth;
 use App\AbstractReportModel;
 use App\Http\Controllers\AbstractReportController;
+
+use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
+use Illuminate\Database\Query\JoinClause;
+use Illuminate\Support\Facades\DB;
 
 class RepoAdwCampaignReportCost extends AbstractReportModel
 {
@@ -18,6 +21,120 @@ class RepoAdwCampaignReportCost extends AbstractReportModel
      * @var boolean
      **/
     public $timestamps = false;
+
+    private function addJoin(EloquentBuilder $builder)
+    {
+        $builder->leftJoin(
+            'phone_time_use',
+            function (JoinClause $join) {
+                $this->addJoinConditions($join);
+            }
+        );
+    }
+
+    protected function addJoinConditions(JoinClause $join)
+    {
+        $join->on('phone_time_use.account_id', '=', $this->table . '.account_id')
+            ->on('phone_time_use.campaign_id', '=', $this->table . '.campaign_id')
+            ->on('phone_time_use.utm_campaign', '=', $this->table . '.campaignID')
+            ->on(
+                DB::raw("STR_TO_DATE(`phone_time_use`.`time_of_call`, '%Y-%m-%d')"),
+                '=',
+                $this->table . '.day'
+            )
+            ->where('phone_time_use.source', '=', 'adw')
+            ->where('phone_time_use.traffic_type', '=', 'AD');
+    }
+
+    /**
+     * @return Expression[]
+     */
+    protected function getAggregatedForTable()
+    {
+        return [
+            DB::raw('COUNT(`phone_time_use`.`id`) AS call_tracking'),
+            DB::raw(
+                "((SUM(`{$this->table}`.`conversions`) + COUNT(`phone_time_use`.`id`)) "
+                . "/ SUM(`{$this->table}`.`clicks`)) * 100 AS call_cvr"
+            ),
+            DB::raw(
+                "SUM(`{$this->table}`.`cost`) / (SUM(`{$this->table}`.`conversions`) "
+                . "+ COUNT(`phone_time_use`.`id`)) AS call_cpa"
+            )
+        ];
+    }
+
+    protected function getBuilderForGetDataForTable(
+        $engine,
+        array $fieldNames,
+        $accountStatus,
+        $startDay,
+        $endDay,
+        $columnSort,
+        $sort,
+        $groupedByField,
+        $accountId = null,
+        $adgainerId = null,
+        $campaignId = null,
+        $adGroupId = null,
+        $adReportId = null,
+        $keywordId = null
+    ) {
+        $builder = parent::getBuilderForGetDataForTable(
+            $engine,
+            $fieldNames,
+            $accountStatus,
+            $startDay,
+            $endDay,
+            $columnSort,
+            $sort,
+            $groupedByField,
+            $accountId,
+            $adgainerId,
+            $campaignId,
+            $adGroupId,
+            $adReportId,
+            $keywordId
+        );
+
+        $this->addJoin($builder);
+
+        return $builder;
+    }
+
+    protected function getBuilderForCalculateData(
+        $engine,
+        $fieldNames,
+        $accountStatus,
+        $startDay,
+        $endDay,
+        $groupedByField,
+        $accountId = null,
+        $adgainerId = null,
+        $campaignId = null,
+        $adGroupId = null,
+        $adReportId = null,
+        $keywordId = null
+    ) {
+        $builder = parent::getBuilderForCalculateData(
+            $engine,
+            $fieldNames,
+            $accountStatus,
+            $startDay,
+            $endDay,
+            $groupedByField,
+            $accountId,
+            $adgainerId,
+            $campaignId,
+            $adGroupId,
+            $adReportId,
+            $keywordId
+        );
+
+        $this->addJoin($builder);
+
+        return $builder;
+    }
 
     public function getAllAdwCampaign(
         $accountId = null
