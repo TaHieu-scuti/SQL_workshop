625fb00e1e35daba67da6119ec1cc02e581f30a7
Youri Westerman <y.westerman@scuti.asia>
2017-12-16 09:54:36 +0700

    Replaced range condition for `time_of_call` with comparison of `day` field

--- database/design/queries/yss-keyword-report-call-tracking.sql
+++ database/design/queries/yss-keyword-report-call-tracking.sql
@@ -91,9 +91,7 @@ FROM
       AND
         `phone_time_use`.`utm_campaign` = `repo_yss_keyword_report_cost`.`campaignID`
       AND
-        `phone_time_use`.`time_of_call` >= '2017-01-01'
-      AND
-        `phone_time_use`.`time_of_call` <= '2017-12-01'
+        STR_TO_DATE(`phone_time_use`.`time_of_call`, '%Y-%m-%d') = `repo_yss_keyword_report_cost`.`day`
       AND
         `phone_time_use`.`source` = 'yss'
       AND
