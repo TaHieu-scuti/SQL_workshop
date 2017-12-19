15530ae71eb6de5cbac060e0c01962b47d2c6322
tungnx <tungxn1594@gmail.com>
2017-12-19 16:13:00 +0700

    update the query

--- database/design/queries/yss-campaign-report-call-tracking.sql
+++ database/design/queries/yss-campaign-report-call-tracking.sql
@@ -9,11 +9,13 @@ SELECT
   AVG(`repo_yss_campaign_report_cost`.`ctr`) AS ctr,
   AVG(`repo_yss_campaign_report_cost`.`averageCpc`) AS avgCPC,
   COUNT(`phone_time_use`.`id`) AS call_tracking,
-  SUM(`repo_yss_campaign_report_cost`.`conversions`) AS webcv,
   SUM(`repo_yss_campaign_report_cost`.`conversions`) + COUNT(`phone_time_use`.`id`) AS cv,
   ((SUM(`repo_yss_campaign_report_cost`.`conversions`) + COUNT(`phone_time_use`.`id`)) / SUM(`repo_yss_campaign_report_cost`.`clicks`)) * 100 AS cvr,
   SUM(`repo_yss_campaign_report_cost`.`cost`) / (SUM(`repo_yss_campaign_report_cost`.`conversions`) + COUNT(`phone_time_use`.`id`)) AS cpa,
-  AVG(`repo_yss_campaign_report_cost`.`averagePosition`) AS avgPosition
+  AVG(`repo_yss_campaign_report_cost`.`averagePosition`) AS avgPosition,
+  SUM(`repo_yss_campaign_report_cost`.`conversions`) AS web_cv,
+  ((SUM(`repo_yss_adgroup_report_cost`.`conversions`) / SUM(`repo_yss_adgroup_report_cost`.`clicks`)) * 100) AS web_cvr,
+  (SUM(`repo_yss_adgroup_report_cost`.`cost`) / SUM(`repo_yss_adgroup_report_cost`.`conversions`)) AS web_cpa
 FROM
   `repo_yss_campaign_report_cost`
     LEFT JOIN `phone_time_use`
