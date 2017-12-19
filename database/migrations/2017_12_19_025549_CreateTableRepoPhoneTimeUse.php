67e70aead4758adb30ef28fdeb2f78133be16571
Youri Westerman <y.westerman@scuti.asia>
2017-12-19 11:06:14 +0700

    Ignoring the fact that laravel migrations are not namespaced

--- database/migrations/2017_12_19_025549_CreateTableRepoPhoneTimeUse.php
+++ database/migrations/2017_12_19_025549_CreateTableRepoPhoneTimeUse.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Migrations\Migration;
 
+// @codingStandardsIgnoreLine
 class CreateTableRepoPhoneTimeUse extends Migration
 {
     /**
