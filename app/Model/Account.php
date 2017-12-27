d91fc1bdaac83939a3aadde430dfd7a44266bddb
= <nguyenchitien195@gmail.com>
2017-12-27 13:46:04 +0700

    fix hidden direct client on breadcrumb when login by normal account

--- app/Model/Account.php
+++ app/Model/Account.php
@@ -666,8 +666,10 @@ class Account extends AbstractReportModel
             && session(AbstractReportController::SESSION_KEY_DIRECT_CLIENT) === 'DirectClient'
         ) {
             return true;
-        } elseif ($title === 'Direct Client'
-            && session(AbstractReportController::SESSION_KEY_DIRECT_CLIENT) === 'Client'
+        } elseif (($title === 'Direct Client'
+            && session(AbstractReportController::SESSION_KEY_DIRECT_CLIENT) === 'Client')
+            || ($title === 'Direct Client'
+            && session(AbstractReportController::SESSION_KEY_DIRECT_CLIENT) === null)
         ) {
             return true;
         }
