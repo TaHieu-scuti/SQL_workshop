d83f150badc6b143bc1e5b5faff7d54693356f30
DuongNguyen3c13 <wanderer.a4@gmail.com>
2017-12-28 03:59:23 +0000

    added model and generator file

---
+++ app/Model/RepoAdwKeywordReportConv.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class RepoAdwKeywordReportConv extends Model
+{
+    protected $table = 'repo_adw_keywords_report_conv';
+
+    public $timestamp = false;
+}
---
+++ database/seeds/RepoAdwKeywordReportConvGenerator.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use App\Model\RepoAdwKeywordReportConv;
+use App\Model\RepoAdwKeywordReportCost;
+
+class RepoAdwKeywordReportConvGenerator extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
