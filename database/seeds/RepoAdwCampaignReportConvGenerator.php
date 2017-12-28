175d1b2ba9bcf4e08fc9606a308b18fc316395de
DuongNguyen3c13 <wanderer.a4@gmail.com>
2017-12-28 07:00:06 +0000

    added adw campaign conv generator

---
+++ database/seeds/RepoAdwCampaignReportConvGenerator.php
@@ -0,0 +1,95 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use App\Model\RepoAdwCampaignReportConv;
+use App\Model\RepoAdwCampaignReportCost;
+
+class RepoAdwCampaignReportConvGenerator extends Seeder
+{
+    const CONVERSION_CATEGORY = [
+        'Conversion category 1',
+        'Conversion category 2',
+        'Conversion category 3',
+        'Conversion category 4'
+    ];
+    const CONVERSION_NAME = [
+        'Conversion name 1',
+        'Conversion name 2',
+        'Conversion name 3',
+        'Conversion name 4'
+    ];
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $campaignCostReports = RepoAdwCampaignReportCost::all();
+        foreach ($campaignCostReports as $campaignCostReport) {
+            $campaignConvReport = new RepoAdwCampaignReportConv;
+
+            $numberOfConversionPoints = mt_rand(1, count(self::CONVERSION_NAME));
+            for ($i = 0; $i < $numberOfConversionPoints; $i++) {
+                $campaignConvReport->exeDate = $campaignCostReport->exeDate;
+                $campaignConvReport->startDate = $campaignCostReport->startDate;
+                $campaignConvReport->endDate = $campaignCostReport->endDate;
+                $campaignConvReport->account_id = $campaignCostReport->account_id;
+                $campaignConvReport->campaign_id = $campaignCostReport->campaign_id;
+                $campaignConvReport->currency = $campaignCostReport->currency;
+                $campaignConvReport->account = $campaignCostReport->account;
+                $campaignConvReport->timeZone = $campaignCostReport->timeZone;
+                $campaignConvReport->network = $campaignCostReport->network;
+                $campaignConvReport->networkWithSearchPartners = $campaignCostReport->networkWithSearchPartners;
+                $campaignConvReport->advertisingSubChannel = $campaignCostReport->advertisingSubChannel;
+                $campaignConvReport->advertisingChannel = $campaignCostReport->advertisingChannel;
+                $campaignConvReport->budget = $campaignCostReport->budget;
+                $campaignConvReport->baseCampaignID = $campaignCostReport->baseCampaignID;
+                $campaignConvReport->bidStrategyID = $campaignCostReport->bidStrategyID;
+                $campaignConvReport->bidStrategyName = $campaignCostReport->bidStrategyName;
+                $campaignConvReport->bidStrategyType = $campaignCostReport->bidStrategyType;
+                $campaignConvReport->conversionOptimizerBidType = $campaignCostReport->conversionOptimizerBidType;
+                $campaignConvReport->budgetID = $campaignCostReport->budgetID;
+                $campaignConvReport->desktopBidAdj = $campaignCostReport->desktopBidAdj;
+                $campaignConvReport->campaignGroupID = $campaignCostReport->campaignGroupID;
+                $campaignConvReport->campaignID = $campaignCostReport->campaignID;
+                $campaignConvReport->mobileBidAdj = $campaignCostReport->mobileBidAdj;
+                $campaignConvReport->campaign = $campaignCostReport->campaign;
+                $campaignConvReport->campaignState = $campaignCostReport->campaignState;
+                $campaignConvReport->tabletBidAdj = $campaignCostReport->tabletBidAdj;
+                $campaignConvReport->campaignTrialType = $campaignCostReport->campaignTrialType;
+                $campaignConvReport->clickType = $campaignCostReport->clickType;
+                $campaignConvReport->conversionCategory = self::CONVERSION_CATEGORY[rand(0, count(self::CONVERSION_CATEGORY) -1)];
+                $campaignConvReport->convRate = $campaignCostReport->convRate;
+                $campaignConvReport->conversions = $campaignCostReport->conversions / $numberOfConversionPoints;
+                $campaignConvReport->convValueCurrentModel = $campaignCostReport->convValueCurrentModel;
+                $campaignConvReport->clientName = $campaignCostReport->clientName;
+                $campaignConvReport->day = $campaignCostReport->day;
+                $campaignConvReport->dayOfWeek = $campaignCostReport->dayOfWeek;
+                $campaignConvReport->device = $campaignCostReport->device;
+                $campaignConvReport->campaignEndDate = $campaignCostReport->campaignEndDate;
+                $campaignConvReport->enhancedCPCEnabled = $campaignCostReport->enhancedCPCEnabled;
+                $campaignConvReport->enhancedCPVEnabled = $campaignCostReport->enhancedCPVEnabled;
+                $campaignConvReport->customerID = $campaignCostReport->customerID;
+                $campaignConvReport->budgetExplicitlyShared = $campaignCostReport->budgetExplicitlyShared;
+                $campaignConvReport->labelIDs = $campaignCostReport->labelIDs;
+                $campaignConvReport->labels = $campaignCostReport->labels;
+                $campaignConvReport->month = $campaignCostReport->month;
+                $campaignConvReport->monthOfYear = $campaignCostReport->monthOfYear;
+                $campaignConvReport->budgetPeriod = $campaignCostReport->budgetPeriod;
+                $campaignConvReport->quarter = $campaignCostReport->quarter;
+                $campaignConvReport->campaignServingStatus = $campaignCostReport->campaignServingStatus;
+                $campaignConvReport->campaignStartDate = $campaignCostReport->campaignStartDate;
+                $campaignConvReport->trackingTemplate = $campaignCostReport->trackingTemplate;
+                $campaignConvReport->customParameter = $campaignCostReport->customParameter;
+                $campaignConvReport->valueConv = $campaignCostReport->valueConv;
+                $campaignConvReport->valueConvCurrentModel = $campaignCostReport->valueConvCurrentModel;
+                $campaignConvReport->week = $campaignCostReport->week;
+                $campaignConvReport->year = $campaignCostReport->year;
+                $campaignConvReport->saveOrFail();
+            }
+        }
+    }
+}
