b193e66c8de0d9571f85c333b1efc7382d95710a
tungnx <tungxn1594@gmail.com>
2017-12-28 13:03:40 +0700

    making seeder for adw account and adgroup conv table

---
+++ database/seeds/RepoAdwAccountConvTableGenerator.php
@@ -0,0 +1,60 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Model\RepoAdwAccountReportCost;
+use App\Model\RepoAdwAccountReportConv;
+
+// @codingStandardsIgnoreLine
+class RepoAdwAccountConvTableGenerator extends Seeder
+{
+    const CONVERSION_CATEGORY = [
+        'Conversion category 1', 'Conversion category 2',
+        'Conversion categroy 3', 'Conversion category 4'
+    ];
+    const CONVERSION_NAME = [
+        'Conversion name 1', 'Conversion name 2',
+        'Conversion name 3', 'Conversion name 4'
+    ];
+    const MIN_CURRENCY = 1;
+    const MAX_CURRENCY = 100;
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $adwCostAccounts = RepoAdwAccountReportCost::all();
+        foreach ($adwCostAccounts as $adwCostAccount) {
+            $adwConvAccount = new RepoAdwAccountReportConv;
+            $adwConvAccount->exeDate = $adwCostAccount->exeDate;
+            $adwConvAccount->startDate = $adwCostAccount->startDate;
+            $adwConvAccount->endDate = $adwCostAccount->endDate;
+            $adwConvAccount->account_id = $adwCostAccount->account_id;
+            $adwConvAccount->campaign_id = $adwCostAccount->campaign_id;
+            $adwConvAccount->currency = mt_rand(self::MIN_CURRENCY, self::MAX_CURRENCY);
+            $adwConvAccount->account = $adwCostAccount->account;
+            $adwConvAccount->timeZone = $adwCostAccount->timeZone;
+            $adwConvAccount->network = $adwCostAccount->network;
+            $adwConvAccount->networkWithSearchPartners = $adwCostAccount->networkWithSearchPartners;
+            $adwConvAccount->conversionCategory = self::CONVERSION_CATEGORY[mt_rand(
+                0,
+                count(self::CONVERSION_CATEGORY) - 1
+            )];
+            $adwConvAccount->conversions = $adwCostAccount->conversions;
+            $adwConvAccount->conversionTrackerId = mt_rand(0, count(self::CONVERSION_NAME) -1);
+            $adwConvAccount->conversionName = self::CONVERSION_NAME[$adwConvAccount->conversionTrackerId];
+            $adwConvAccount->day = $adwCostAccount->day;
+            $adwConvAccount->dayOfWeek = $adwCostAccount->dayOfWeek;
+            $adwConvAccount->device = $adwCostAccount->device;
+            $adwConvAccount->customerID = $adwCostAccount->customerID;
+            $adwConvAccount->hourOfDay = $adwCostAccount->hourOfDay;
+            $adwConvAccount->month = $adwCostAccount->month;
+            $adwConvAccount->monthOfYear = $adwCostAccount->monthOfYear;
+            $adwConvAccount->quarter = $adwCostAccount->quarter;
+            $adwConvAccount->week = $adwCostAccount->week;
+            $adwConvAccount->year = $adwCostAccount->year;
+            $adwConvAccount->saveOrFail();
+        }
+    }
+}
---
+++ database/seeds/RepoAdwAdgroupConvTableGenerator.php
@@ -0,0 +1,73 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use App\Model\RepoAdwAdgroupReportCost;
+use App\Model\RepoAdwAdgroupReportConv;
+
+// @codingStandardsIgnoreLine
+class RepoAdwAdgroupConvTableGenerator extends Seeder
+{
+    const CONVERSION_CATEGORY = [
+        'Conversion category 1', 'Conversion category 2',
+        'Conversion categroy 3', 'Conversion category 4'
+    ];
+    const CONVERSION_NAME = [
+        'Conversion name 1', 'Conversion name 2',
+        'Conversion name 3', 'Conversion name 4'
+    ];
+    const MIN_CURRENCY = 1;
+    const MAX_CURRENCY = 100;
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $adwCostAdgroups = RepoAdwAdgroupReportCost::all();
+        foreach ($adwCostAdgroups as $adwCostAdgroup) {
+            $adwConvAdgroup = new RepoAdwAdgroupReportConv;
+            $adwConvAdgroup->exeDate = $adwCostAdgroup->exeDate;
+            $adwConvAdgroup->startDate = $adwCostAdgroup->startDate;
+            $adwConvAdgroup->endDate = $adwCostAdgroup->endDate;
+            $adwConvAdgroup->account_id = $adwCostAdgroup->account_id;
+            $adwConvAdgroup->campaign_id = $adwCostAdgroup->campaign_id;
+            $adwConvAdgroup->currency = mt_rand(
+                self::MIN_CURRENCY,
+                self::MAX_CURRENCY
+            );
+            $adwConvAdgroup->account = $adwCostAdgroup->account;
+            $adwConvAdgroup->timeZone = $adwCostAdgroup->timeZone;
+            $adwConvAdgroup->adGroupID = $adwCostAdgroup->adGroupID;
+            $adwConvAdgroup->adGroup = $adwCostAdgroup->adGroup;
+            $adwConvAdgroup->adGroupState = $adwCostAdgroup->adGroupState;
+            $adwConvAdgroup->adGroupType = $adwCostAdgroup->adGroupType;
+            $adwConvAdgroup->network = $adwCostAdgroup->network;
+            $adwConvAdgroup->networkWithSearchPartners = $adwCostAdgroup->networkWithSearchPartners;
+            $adwConvAdgroup->baseAdGroupID = $adwCostAdgroup->baseAdGroupID;
+            $adwConvAdgroup->baseCampaignID = $adwCostAdgroup->baseCampaignID;
+            $adwConvAdgroup->campaignID = $adwCostAdgroup->campaignID;
+            $adwConvAdgroup->campaign = $adwCostAdgroup->campaign;
+            $adwConvAdgroup->campaignState = $adwCostAdgroup->campaignState;
+            $adwConvAdgroup->conversionCategory = self::CONVERSION_CATEGORY[mt_rand(
+                0,
+                count(self::CONVERSION_CATEGORY) - 1
+            )];
+            $adwConvAdgroup->conversionTrackerId = mt_rand(0, count(self::CONVERSION_NAME) -1);
+            $adwConvAdgroup->conversionName = self::CONVERSION_NAME[$adwConvAdgroup->conversionTrackerId];
+            $adwConvAdgroup->clientName = $adwCostAdgroup->clientName;
+            $adwConvAdgroup->day = $adwCostAdgroup->day;
+            $adwConvAdgroup->dayOfWeek = $adwCostAdgroup->dayOfWeek;
+            $adwConvAdgroup->device = $adwCostAdgroup->device;
+            $adwConvAdgroup->customerID = $adwCostAdgroup->customerID;
+            $adwConvAdgroup->hourOfDay = $adwCostAdgroup->hourOfDay;
+            $adwConvAdgroup->month = $adwCostAdgroup->month;
+            $adwConvAdgroup->monthOfYear = $adwCostAdgroup->monthOfYear;
+            $adwConvAdgroup->quarter = $adwCostAdgroup->quarter;
+            $adwConvAdgroup->week = $adwCostAdgroup->week;
+            $adwConvAdgroup->year = $adwCostAdgroup->year;
+            $adwConvAdgroup->saveOrFail();
+        }
+    }
+}
