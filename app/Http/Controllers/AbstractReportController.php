027086272bfcafb7acf799a5f43ef257fb319502
= <nguyenchitien195@gmail.com>
2018-01-02 16:05:33 +0700

    fix bug when sort direct clients column on table on direct client report page

--- app/Http/Controllers/AbstractReportController.php
+++ app/Http/Controllers/AbstractReportController.php
@@ -656,6 +656,7 @@ abstract class AbstractReportController extends Controller
         if (!in_array(session(static::SESSION_KEY_COLUMN_SORT), session(static::SESSION_KEY_FIELD_NAME))) {
             if (session(static::SESSION_KEY_COLUMN_SORT) !== 'agencyName'
                 && session(static::SESSION_KEY_COLUMN_SORT) !== 'clientName'
+                && session(static::SESSION_KEY_COLUMN_SORT) !== 'directClients'
             ) {
                 session([static::SESSION_KEY_COLUMN_SORT => session(static::SESSION_KEY_FIELD_NAME)[0]]);
             }
