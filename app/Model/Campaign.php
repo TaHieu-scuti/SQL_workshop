1355d652b09c47e1886009f776ce97b22720b948
Youri Westerman <y.westerman@scuti.asia>
2018-01-03 17:50:29 +0700

    Added method to retrieve all adgainer campaigns with phone_number from the database

--- app/Model/Campaign.php
+++ app/Model/Campaign.php
@@ -3,9 +3,49 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Query\JoinClause;
 
 class Campaign extends Model
 {
     /** @var bool */
     public $timestamps = false;
+
+    public function getAdGainerCampaignsWithPhoneNumber($account_id, $source, array $campaignIDs)
+    {
+        $campaignsTableName = $this->getTable();
+        $repoPhoneTimeUseTableName = (new RepoPhoneTimeUse)->getTable();
+        return $this->distinct()
+            ->select([
+                $campaignsTableName . '.campaign_id',
+                $campaignsTableName . '.campaign_name',
+                $repoPhoneTimeUseTableName . '.utm_campaign',
+                $repoPhoneTimeUseTableName . '.phone_number'
+            ])
+            ->join(
+                $repoPhoneTimeUseTableName,
+                function (JoinClause $join) use ($campaignsTableName, $repoPhoneTimeUseTableName) {
+                    $join->on(
+                        $campaignsTableName . '.account_id',
+                        '=',
+                        $repoPhoneTimeUseTableName . '.account_id'
+                    )->on(
+                        $campaignsTableName . '.campaign_id',
+                        '=',
+                        $repoPhoneTimeUseTableName . '.campaign_id'
+                    );
+                }
+            )
+            ->where(
+                $campaignsTableName . '.account_id',
+                '=',
+                $account_id
+            )
+            ->where(
+                $repoPhoneTimeUseTableName . '.source',
+                '=',
+                $source
+            )
+            ->whereIn($repoPhoneTimeUseTableName . '.utm_campaign', $campaignIDs)
+            ->get();
+    }
 }
