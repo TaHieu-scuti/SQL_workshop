f093a62cb8577aa0d7a2d17e15c1188afc91042a
Youri Westerman <y.westerman@scuti.asia>
2018-01-03 12:10:55 +0700

    Adding more fields to repo_phone_time_use

---
+++ database/migrations/2018_01_03_050059_AddFieldsToRepoPhoneTimeUse.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+// @codingStandardsIgnoreLine
+class AddFieldsToRepoPhoneTimeUse extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table(
+            'repo_phone_time_use',
+            function (Blueprint $table) {
+                $table->string('account_id', 50);
+                $table->string('campaign_id', 50);
+                $table->mediumText('utm_campaign');
+                $table->string('time_of_call', 255);
+                $table->string('source', 255);
+            }
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table(
+            'repo_phone_time_use',
+            function (Blueprint $table) {
+                $table->dropColumn([
+                    'account_id',
+                    'campaign_id',
+                    'utm_campaign',
+                    'time_of_call',
+                    'source'
+                ]);
+            }
+        );
+    }
+}
