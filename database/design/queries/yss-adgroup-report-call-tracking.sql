8a7fc425ea2e21b15e34ccedee7e6e87dd5b07c3
Youri Westerman <y.westerman@scuti.asia>
2018-01-08 13:30:26 +0700

    Updated query to include the call tracking per phone number and the conversion points from the conv table

--- database/design/queries/yss-adgroup-report-call-tracking.sql
+++ database/design/queries/yss-adgroup-report-call-tracking.sql
@@ -1,116 +1,248 @@
-/* CONFIRMED TO BE WORKING CORRECTLY */
+/* Get all unique conversion names */
 SELECT
-  `repo_yss_adgroup_report_cost`.`account_id`,
-  `repo_yss_adgroup_report_cost`.`accountid`,
-  `repo_yss_adgroup_report_cost`.`campaignID`,
-  `repo_yss_adgroup_report_cost`.`adgroupID`,
-  SUM(`repo_yss_adgroup_report_cost`.`impressions`) AS impressions,
-  SUM(`repo_yss_adgroup_report_cost`.`clicks`) AS clicks,
-  SUM(`repo_yss_adgroup_report_cost`.`cost`) AS cost,
-  AVG(`repo_yss_adgroup_report_cost`.`ctr`) AS ctr,
-  AVG(`repo_yss_adgroup_report_cost`.`averageCpc`) AS avgCPC,
-  COUNT(`phone_time_use`.`id`) AS call_cv,
-  SUM(`repo_yss_adgroup_report_cost`.`conversions`) + COUNT(`phone_time_use`.`id`) AS cv,
-  ((SUM(`repo_yss_adgroup_report_cost`.`conversions`) + COUNT(`phone_time_use`.`id`)) / SUM(`repo_yss_adgroup_report_cost`.`clicks`)) * 100 AS cvr,
-  SUM(`repo_yss_adgroup_report_cost`.`cost`) / (SUM(`repo_yss_adgroup_report_cost`.`conversions`) + COUNT(`phone_time_use`.`id`)) AS cpa,
-  AVG(`repo_yss_adgroup_report_cost`.`averagePosition`) AS avgPosition,
-  SUM(`repo_yss_adgroup_report_cost`.`conversions`) AS web_cv,
-  ((SUM(`repo_yss_adgroup_report_cost`.`conversions`) / SUM(`repo_yss_adgroup_report_cost`.`clicks`)) * 100) AS web_cvr,
-  (SUM(`repo_yss_adgroup_report_cost`.`cost`) / SUM(`repo_yss_adgroup_report_cost`.`conversions`)) AS web_cpa,
+  DISTINCT
+  `adgroupID`,
+  `conversionName`
 FROM
-  `repo_yss_adgroup_report_cost`
-    LEFT JOIN (`campaigns`, `phone_time_use`)
-    ON (
-        `campaigns`.`account_id` = `repo_yss_adgroup_report_cost`.`account_id`
-      AND
-        `campaigns`.`campaign_id` = `repo_yss_adgroup_report_cost`.`campaign_id`
-      AND
-        (
-          (
-            `campaigns`.`camp_custom1` = 'adgroupid'
-          AND
-            `phone_time_use`.`custom1` = `repo_yss_adgroup_report_cost`.`adgroupID`
-          )
-        OR
-          (
-            `campaigns`.`camp_custom2` = 'adgroupid'
-          AND
-            `phone_time_use`.`custom2` = `repo_yss_adgroup_report_cost`.`adgroupID`
-          )
-        OR
-          (
-            `campaigns`.`camp_custom3` = 'adgroupid'
-          AND
-            `phone_time_use`.`custom3` = `repo_yss_adgroup_report_cost`.`adgroupID`
-          )
-        OR
-          (
-            `campaigns`.`camp_custom4` = 'adgroupid'
-          AND
-            `phone_time_use`.`custom4` = `repo_yss_adgroup_report_cost`.`adgroupID`
-          )
-        OR
-          (
-            `campaigns`.`camp_custom5` = 'adgroupid'
-          AND
-            `phone_time_use`.`custom5` = `repo_yss_adgroup_report_cost`.`adgroupID`
-          )
-        OR
-          (
-            `campaigns`.`camp_custom6` = 'adgroupid'
-          AND
-            `phone_time_use`.`custom6` = `repo_yss_adgroup_report_cost`.`adgroupID`
-          )
-        OR
-          (
-            `campaigns`.`camp_custom7` = 'adgroupid'
-          AND
-            `phone_time_use`.`custom7` = `repo_yss_adgroup_report_cost`.`adgroupID`
-          )
-        OR
-          (
-            `campaigns`.`camp_custom8` = 'adgroupid'
-          AND
-            `phone_time_use`.`custom8` = `repo_yss_adgroup_report_cost`.`adgroupID`
-          )
-        OR
-          (
-            `campaigns`.`camp_custom9` = 'adgroupid'
-          AND
-            `phone_time_use`.`custom9` = `repo_yss_adgroup_report_cost`.`adgroupID`
-          )
-        OR
-          (
-            `campaigns`.`camp_custom10` = 'adgroupid'
-          AND
-            `phone_time_use`.`custom10` = `repo_yss_adgroup_report_cost`.`adgroupID`
-          )
-        )
-      AND
-        `phone_time_use`.`account_id` = `repo_yss_adgroup_report_cost`.`account_id`
-      AND
-        `phone_time_use`.`campaign_id` = `repo_yss_adgroup_report_cost`.`campaign_id`
-      AND
-        `phone_time_use`.`utm_campaign` = `repo_yss_adgroup_report_cost`.`campaignID`
-      AND
-        STR_TO_DATE(`phone_time_use`.`time_of_call`, '%Y-%m-%d') = `repo_yss_adgroup_report_cost`.`day`
-      AND
-        `phone_time_use`.`source` = 'yss'
-      AND
-        `phone_time_use`.`traffic_type` = 'AD'
-    )
+  `repo_yss_adgroup_report_conv`
 WHERE
-  `repo_yss_adgroup_report_cost`.`account_id` = 1
+  `account_id` = 1
+  AND
+  `accountid` = 11
+  AND
+  `campaignID` = 11;
+/* Query result:
+|adgroupID|conversionName       |
+|11111111 |YSS conversion 111110|
+|11111111 |YSS conversion 111111|
+|11111111 |YSS conversion 111112|
+|11111112 |YSS conversion 111110|
+|11111112 |YSS conversion 111111|
+|11111112 |YSS conversion 111112|
+|11111113 |YSS conversion 111110|
+|11111113 |YSS conversion 111111|
+|11111113 |YSS conversion 111112|
+|11111114 |YSS conversion 111110|
+|11111114 |YSS conversion 111111|
+|11111114 |YSS conversion 111112|
+*/
+
+/* Get all AG campaigns with phone number */
+SELECT
+  DISTINCT
+  `c`.`campaign_id`,
+  `c`.`campaign_name`,
+  `ptu`.`utm_campaign`,
+  `ptu`.`phone_number`
+FROM
+  `campaigns` c,
+  `phone_time_use` ptu
+WHERE
+  `c`.`campaign_id` = `ptu`.`campaign_id`
+  AND
+  `c`.`account_id` = `ptu`.`account_id`
+  AND
+  `c`.`account_id` = 1
+  AND
+  `ptu`.`utm_campaign` = 11
+  AND
+  `ptu`.`source` = 'yss';
+/* Query results:
+|campaign_id|campaign_name|utm_campaign|phone_number |
+|11         |Campaign Name|11          |+841234567811|
+*/
+
+SELECT
+  `total`.`account_id`,
+  `total`.`accountid`,
+  `total`.`campaignID`,
+  `total`.`adgroupID`,
+  IFNULL(SUM(`total`.`impressions`), 0) AS impressions,
+  IFNULL(SUM(`total`.`clicks`), 0) AS clicks,
+  IFNULL(SUM(`total`.`cost`), 0) AS cost,
+  IFNULL(AVG(`total`.`ctr`), 0) AS ctr,
+  IFNULL(AVG(`total`.`averageCpc`), 0) AS avgCPC,
+  IFNULL(AVG(`total`.`averagePosition`), 0) AS avgPosition,
+  IFNULL(SUM(`conv0`.`conversions`), 0) AS 'YSS conversion 111110 CV',
+  IFNULL((SUM(`conv0`.`conversions`) / SUM(`total`.`clicks`)) * 100, 0) AS 'YSS conversion 111110 CVR',
+  IFNULL((SUM(`total`.`cost`) / SUM(`conv0`.`conversions`)) * 100, 0) AS 'YSS conversion 111110 CPA',
+  IFNULL(SUM(`conv1`.`conversions`), 0) AS 'YSS conversion 111111 CV',
+  IFNULL((SUM(`conv1`.`conversions`) / SUM(`total`.`clicks`)) * 100, 0) AS 'YSS conversion 111111 CVR',
+  IFNULL((SUM(`total`.`cost`) / SUM(`conv1`.`conversions`)) * 100, 0) AS 'YSS conversion 111111 CPA',
+  IFNULL(SUM(`conv2`.`conversions`), 0) AS 'YSS conversion 111112 CV',
+  IFNULL((SUM(`conv2`.`conversions`) / SUM(`total`.`clicks`)) * 100, 0) AS 'YSS conversion 111112 CVR',
+  IFNULL((SUM(`total`.`cost`) / SUM(`conv2`.`conversions`)) * 100, 0) AS 'YSS conversion 111112 CPA',
+  IFNULL(`total`.`conversions`, 0) AS web_cv,
+  IFNULL(`total`.`conversions` / `total`.`clicks`, 0) AS web_cvr,
+  IFNULL(`total`.`cost` / `total`.`conversions`, 0) AS web_cpa,
+  IFNULL(COUNT(`call0_ptu`.`id`), 0) AS 'Campaign Name +841234567811 CV',
+  IFNULL((COUNT(`call0_ptu`.`id`) / `total`.`clicks`) * 100, 0) AS 'Campaign Name +841234567811 CVR',
+  IFNULL(`total`.`cost` / COUNT(`call0_ptu`.`id`), 0) AS 'Campaign Name +841234567811 CPA',
+  IFNULL(COUNT(`call0_ptu`.`id`), 0) AS call_cv,
+  IFNULL((COUNT(`call0_ptu`.`id`) / `total`.`clicks`) * 100, 0) AS call_cvr,
+  IFNULL(`total`.`cost` / COUNT(`call0_ptu`.`id`), 0) AS call_cpa,
+  IFNULL(`total`.`conversions` + COUNT(`call0_ptu`.`id`), 0) AS total_cv,
+  IFNULL((`total`.`conversions` + COUNT(`call0_ptu`.`id`)) / `total`.`clicks`, 0) AS total_cvr,
+  IFNULL(`total`.`cost` / (`total`.`conversions` + COUNT(`call0_ptu`.`id`)), 0) AS total_cpa
+FROM
+  `repo_yss_adgroup_report_cost` AS total
+  /* Add joins for the conversion points */
+    LEFT JOIN `repo_yss_adgroup_report_conv` AS conv0
+      ON (
+          `total`.`account_id` = `conv0`.`account_id`
+        AND
+          `total`.`accountid` = `conv0`.`accountid`
+        AND
+          `total`.`day` = `conv0`.`day`
+        AND
+          `total`.`campaignID` = `conv0`.`campaignID`
+        AND (
+            `conv0`.`adgroupID` = 11111111
+          OR
+            `conv0`.`adgroupID` = 11111112
+          OR
+            `conv0`.`adgroupID` = 11111113
+          OR
+            `conv0`.`adgroupID` = 11111114
+        )
+        AND
+        `conv0`.`conversionName` = 'YSS conversion 111110'
+      )
+    LEFT JOIN `repo_yss_adgroup_report_conv` AS conv1
+      ON (
+          `total`.`account_id` = `conv1`.`account_id`
+        AND
+          `total`.`accountid` = `conv1`.`accountid`
+        AND
+          `total`.`day` = `conv1`.`day`
+        AND
+          `total`.`campaignID` = `conv1`.`campaignID`
+        AND (
+            `conv1`.`adgroupID` = 11111111
+          OR
+            `conv1`.`adgroupID` = 11111112
+          OR
+            `conv1`.`adgroupID` = 11111113
+          OR
+            `conv1`.`adgroupID` = 11111114
+        )
+        AND
+          `conv1`.`conversionName` = 'YSS conversion 111111'
+      )
+    LEFT JOIN `repo_yss_adgroup_report_conv` AS conv2
+      ON (
+          `total`.`account_id` = `conv2`.`account_id`
+        AND
+          `total`.`accountid` = `conv2`.`accountid`
+        AND
+          `total`.`day` = `conv2`.`day`
+        AND
+          `total`.`campaignID` = `conv2`.`campaignID`
+        AND (
+            `conv2`.`adgroupID` = 11111111
+          OR
+            `conv2`.`adgroupID` = 11111112
+          OR
+            `conv2`.`adgroupID` = 11111113
+          OR
+            `conv2`.`adgroupID` = 11111114
+        )
+        AND
+          `conv2`.`conversionName` = 'YSS conversion 111112'
+      )
+    /* Add joins for every AG campaign & phone_number combination */
+    LEFT JOIN (`campaigns` AS call0_campaigns, `phone_time_use` AS call0_ptu)
+      ON (
+            `call0_campaigns`.`account_id` = `total`.`account_id`
+          AND
+            `call0_campaigns`.`campaign_id` = `total`.`campaign_id`
+          AND
+            (
+              (
+                `call0_campaigns`.`camp_custom1` = 'adgroupid'
+              AND
+                `call0_ptu`.`custom1` = `total`.`adgroupID`
+              )
+            OR
+              (
+                `call0_campaigns`.`camp_custom2` = 'adgroupid'
+              AND
+                `call0_ptu`.`custom2` = `total`.`adgroupID`
+              )
+            OR
+              (
+                `call0_campaigns`.`camp_custom3` = 'adgroupid'
+              AND
+                `call0_ptu`.`custom3` = `total`.`adgroupID`
+              )
+            OR
+              (
+                `call0_campaigns`.`camp_custom4` = 'adgroupid'
+              AND
+                `call0_ptu`.`custom4` = `total`.`adgroupID`
+              )
+            OR
+              (
+                `call0_campaigns`.`camp_custom5` = 'adgroupid'
+              AND
+                `call0_ptu`.`custom5` = `total`.`adgroupID`
+              )
+            OR
+              (
+                `call0_campaigns`.`camp_custom6` = 'adgroupid'
+              AND
+                `call0_ptu`.`custom6` = `total`.`adgroupID`
+              )
+            OR
+              (
+                `call0_campaigns`.`camp_custom7` = 'adgroupid'
+              AND
+                `call0_ptu`.`custom7` = `total`.`adgroupID`
+              )
+            OR
+              (
+                `call0_campaigns`.`camp_custom8` = 'adgroupid'
+              AND
+                `call0_ptu`.`custom8` = `total`.`adgroupID`
+              )
+            OR
+              (
+                `call0_campaigns`.`camp_custom9` = 'adgroupid'
+              AND
+                `call0_ptu`.`custom9` = `total`.`adgroupID`
+              )
+            OR
+              (
+                `call0_campaigns`.`camp_custom10` = 'adgroupid'
+              AND
+                `call0_ptu`.`custom10` = `total`.`adgroupID`
+              )
+            )
+        AND
+          `call0_ptu`.`account_id` = `total`.`account_id`
+        AND
+          `call0_ptu`.`campaign_id` = `total`.`campaign_id`
+        AND
+          `call0_ptu`.`utm_campaign` = `total`.`campaignID`
+        AND
+          STR_TO_DATE(`call0_ptu`.`time_of_call`, '%Y-%m-%d') = `total`.`day`
+        AND
+          `call0_ptu`.`source` = 'yss'
+        AND
+          `call0_ptu`.`traffic_type` = 'AD'
+      )
+WHERE
+  `total`.`account_id` = 1
 AND
-  `repo_yss_adgroup_report_cost`.`accountid` = 11111
+  `total`.`accountid` = 11
 AND
-  `repo_yss_adgroup_report_cost`.`campaignID` = 111111
+  `total`.`campaignID` = 11
 AND
-  `repo_yss_adgroup_report_cost`.`day` >= '2017-01-01'
+  `total`.`day` >= '2017-01-01'
 AND
-  `repo_yss_adgroup_report_cost`.`day` <= '2017-12-01'
+  `total`.`day` <= '2017-12-01'
 GROUP BY
-  `repo_yss_adgroup_report_cost`.`account_id`,
-  `repo_yss_adgroup_report_cost`.`accountid`,
-  `repo_yss_adgroup_report_cost`.`campaignID`,
-  `repo_yss_adgroup_report_cost`.`adgroupID`
+  `total`.`account_id`,
+  `total`.`accountid`,
+  `total`.`campaignID`,
+  `total`.`adgroupID`
