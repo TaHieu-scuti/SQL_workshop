9342fff2910db2ba874ed26dfc6de87ae19b1de3
hieutq-scuti <hieutq@scuti.asia>
2018-01-10 13:12:05 +0700

    add index for repo ydn reports

---
+++ database/migrations/2018_01_10_050421_AddIndexConversionNameOnRepoYdnReports.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddIndexConversionNameOnRepoYdnReports extends Migration
+{
+    const INDEX_NAME = 'repo_ydn_reports_convName_day_convName_idx';
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement(
+            "CREATE INDEX `" . self::INDEX_NAME . "` "
+            . "ON `repo_ydn_reports` (conversionName(100), day, conversionName) "
+            . "COMMENT '' "
+            . "ALGORITHM DEFAULT "
+            . "LOCK DEFAULT;"
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table(
+            'repo_ydn_reports',
+            function (Blueprint $table) {
+                $table->dropIndex(self::INDEX_NAME);
+            }
+        );
+    }
+}
