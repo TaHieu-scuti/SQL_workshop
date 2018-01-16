acb2d384af7f0a55443cd403db0e89a232af9622
Youri Westerman <y.westerman@scuti.asia>
2018-01-16 10:53:13 +0700

    Added index on campaignID and day in table repo_adw_adgroup_report_cost

---
+++ database/migrations/2018_01_16_035017_AddIndexCampaignIDDayADWAdGroupCost.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddIndexCampaignIDDayADWAdGroupCost extends Migration
+{
+    const TABLE_NAME = 'repo_adw_adgroup_report_cost';
+    const INDEX_NAME = 'repo_adw_adgroup_report_cost_campaignID_day';
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table(
+            self::TABLE_NAME,
+            function (Blueprint $table) {
+                $table->index(['campaignID', 'day'], self::INDEX_NAME);
+            }
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table(
+            self::TABLE_NAME,
+            function (Blueprint $table) {
+                $table->dropIndex(self::INDEX_NAME);
+            }
+        );
+    }
+}
