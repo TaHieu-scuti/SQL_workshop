e7ac8855faefa8e106dd41fbf90c583d73e03445
tiennc <nguyenchitien195@gmail.com>
2018-01-23 14:22:58 +0700

    implement query for yss campaign conversion points day of week

--- app/Model/RepoPhoneTimeUse.php
+++ app/Model/RepoPhoneTimeUse.php
@@ -13,4 +13,22 @@ class RepoPhoneTimeUse extends Model
     {
         return $this->belongsTo('App\Model\PhoneTimeUse');
     }
+
+    public function getPhoneTimeUseWithDayOfWeek(
+        $account_id,
+        $source,
+        array $campaignIDs
+    ) {
+        return $this->distinct()
+        ->select(
+            'account_id',
+            'utm_campaign',
+            'phone_number'
+        )->selectRaw('DAYNAME(time_of_call) AS `dayOfWeek`')
+        ->where('repo_phone_time_use.account_id', $account_id)
+        ->whereIn('repo_phone_time_use.utm_campaign', $campaignIDs)
+        ->where('repo_phone_time_use.source', $source)
+        ->where('repo_phone_time_use.traffic_type', 'AD')
+        ->get();
+    }
 }
