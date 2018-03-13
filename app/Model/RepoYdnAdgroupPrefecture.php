423fb58f0615bf6826569c4010b2b499df5bb5f0
DuongNguyen3c13 <wanderer.a4@gmail.com>
2018-03-13 12:14:42 +0000

    added function for handling adding and updating temporary model

--- app/Model/RepoYdnAdgroupPrefecture.php
+++ app/Model/RepoYdnAdgroupPrefecture.php
@@ -2,8 +2,12 @@
 
 namespace App\Model;
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
+use Illuminate\Database\Query\JoinClause;
+
 use App\Model\AbstractYdnReportModel;
-use App
+use App\AbstractReportModel;
 
 class RepoYdnAdgroupPrefecture extends AbstractYdnReportModel
 {
@@ -11,7 +15,7 @@ class RepoYdnAdgroupPrefecture extends AbstractYdnReportModel
 
     public $timestamps = false;
 
-    const PAGE_ID = 'adgroupId';
+    const PAGE_ID = 'adgroupID';
 
     protected function getBuilderForGetDataForTable(
         $engine,
@@ -71,7 +75,7 @@ class RepoYdnAdgroupPrefecture extends AbstractYdnReportModel
             if (($key = array_search(self::PAGE_ID, $columns)) !== false) {
                 unset($columns[$key]);
             }
-            if (($key = array_search('campaignName', $columns)) !== false) {
+            if (($key = array_search('adgroupName', $columns)) !== false) {
                 unset($columns[$key]);
             }
 
@@ -127,4 +131,100 @@ class RepoYdnAdgroupPrefecture extends AbstractYdnReportModel
         return $builder;
     }
 
+    protected function updateTemporaryTableWithConversion(
+        $conversionPoints,
+        $groupedByField,
+        $startDay,
+        $endDay,
+        $engine,
+        $clientId = null,
+        $accountId = null,
+        $campaignId = null,
+        $adGroupId = null,
+        $adReportId = null,
+        $keywordId = null
+    ) {
+        $conversionNames = array_values(array_unique($conversionPoints->pluck('conversionName')->toArray()));
+        foreach ($conversionNames as $key => $conversionName) {
+            $queryGetConversion = $this->select(
+                DB::raw('SUM(repo_ydn_reports.conversions) AS conversions, '.$groupedByField)
+            )->where('conversionName', $conversionName)
+            ->where(
+                function (EloquentBuilder $query) use (
+                    $startDay,
+                    $endDay,
+                    $engine,
+                    $clientId,
+                    $accountId,
+                    $campaignId,
+                    $adGroupId,
+                    $adReportId,
+                    $keywordId
+                ) {
+                    $this->getCondition(
+                        $query,
+                        $startDay,
+                        $endDay,
+                        $engine,
+                        $clientId,
+                        $accountId,
+                        $campaignId,
+                        $adGroupId,
+                        $adReportId,
+                        $keywordId
+                    );
+                }
+            )->groupBy($groupedByField);
+
+            DB::update(
+                'update '.self::TABLE_TEMPORARY.', ('
+                .$this->getBindingSql($queryGetConversion).')AS tbl set conversions'.$key.' = tbl.conversions where '
+                .self::TABLE_TEMPORARY.'.'.$groupedByField.' = tbl.'.$groupedByField
+            );
+        }
+    }
+
+        protected function updateTemporaryTableWithCallTracking(
+        $adGainerCampaigns,
+        $groupedByField,
+        $startDay,
+        $endDay,
+        $engine,
+        $clientId = null,
+        $accountId = null,
+        $campaignId = null,
+        $adGroupId = null,
+        $adReportId = null,
+        $keywordId = null
+    ) {
+        $utmCampaignList = array_unique($adGainerCampaigns->pluck('utm_campaign')->toArray());
+
+        $phoneNumbers = array_values(array_unique($adGainerCampaigns->pluck('phone_number')->toArray()));
+
+        $phoneTimeUseModel = new PhoneTimeUse();
+        $phoneTimeUseTableName = $phoneTimeUseModel->getTable();
+        foreach ($phoneNumbers as $i => $phoneNumber) {
+            $builder = $phoneTimeUseModel->select(
+                [
+                    DB::raw('count(id) AS id'),
+                    'visitor_city_state'
+                ]
+            )
+                ->where('source', '=', $engine)
+                ->whereRaw('traffic_type = "AD"')
+                ->whereIn('phone_number', $phoneNumbers)
+                ->whereIn('utm_campaign', $utmCampaignList)
+                ->where(
+                    function (EloquentBuilder $query) use ($startDay, $endDay, $phoneTimeUseTableName) {
+                        $this->addConditonForDate($query, $phoneTimeUseTableName, $startDay, $endDay);
+                    }
+                )
+                ->groupBy('visitor_city_state');
+            DB::update(
+                'update '.self::TABLE_TEMPORARY.', ('
+                .$this->getBindingSql($builder).') AS tbl set call'.$i.' = tbl.id where '
+                .'tbl.visitor_city_state LIKE CONCAT("%",'.self::TABLE_TEMPORARY.'.prefecture," (Japan)")'
+            );
+        }
+    }
 }
