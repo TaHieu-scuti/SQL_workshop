f0a64358977082324c27906fdc211371ffc5fe72
TungNguyenLelele <tungxn1594@gmail.com>
2018-03-15 09:50:37 +0700

    add AbstractAdwPrefecture model

---
+++ app/Model/AbstractAdwPrefecture.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Model;
+
+use App\Model\AbstractAdwSubReportModel;
+
+abstract class AbstractAdwPrefecture extends AbstractAdwSubReportModel
+{
+    protected function getBuilderForGetDataForTable(
+        $engine,
+        array $fieldNames,
+        $accountStatus,
+        $startDay,
+        $endDay,
+        $columnSort,
+        $sort,
+        $groupedByField,
+        $agencyId = null,
+        $accountId = null,
+        $clientId = null,
+        $campaignId = null,
+        $adGroupId = null,
+        $adReportId = null,
+        $keywordId = null
+    ) {
+        
+    }
+}
