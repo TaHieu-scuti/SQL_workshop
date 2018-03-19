80374a44eaafa71c71a7c05d6be7e8ebdd2aa661
Youri Westerman <y.westerman@scuti.asia>
2018-03-19 13:14:01 +0700

    Registering the middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\SaveDatabaseConnectionIdToSession;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -60,5 +61,6 @@ class Kernel extends HttpKernel
         'engine' =>\App\Http\Middleware\Engine::class,
         'checkRole' => \App\Http\Middleware\CheckRole::class,
         'checkRoleClient' => \App\Http\Middleware\CheckRoleClient::class,
+        'saveDatabaseConnectionIdToSession' => SaveDatabaseConnectionIdToSession::class
     ];
 }
