c392dd58a5db131ce8f440d79cf00f71559e9313
Youri Westerman <y.westerman@scuti.asia>
2018-03-19 13:13:23 +0700

    More work in progress

--- app/Http/Middleware/SaveDatabaseConnectionIdToSession.php
+++ app/Http/Middleware/SaveDatabaseConnectionIdToSession.php
@@ -6,6 +6,7 @@ use Closure;
 use Illuminate\Database\Connection;
 use Illuminate\Database\MySqlConnection;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Database\QueryException;
 
 class SaveDatabaseConnectionIdToSession
 {
@@ -23,7 +24,7 @@ class SaveDatabaseConnectionIdToSession
     }
 
     /**
-     * Query for
+     *
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Closure  $next
@@ -31,14 +32,39 @@ class SaveDatabaseConnectionIdToSession
      */
     public function handle($request, Closure $next)
     {
-        Log::debug('SaveDatabaseConnectionIdToSession');
-
         if ($request->isXmlHttpRequest()) {
-            /*var_dump($request->path());
-            dd($request->session()->all());*/
-            // TODO use full uri for the connection id's and cancel the old one if it exists
+            Log::debug(getmypid() . ': Request vars: ', $request->all());
+            $session = $request->session();
+            Log::debug(getmypid() . ': SaveDatabaseConnectionIdToSession ' . $request->path());
+            Log::debug(getmypid() . ': current session vars', $session->get(self::SESSION_KEY_MYSQL_CONNECTION_IDS, []));
+
+            $session = $request->session();
+
             $connectionId = $this->connection->select('SELECT CONNECTION_ID() as id')[0]->id;
-            $request->session()->push(self::SESSION_KEY_MYSQL_CONNECTION_IDS, $connectionId);
+            Log::debug(getmypid() . ': Current connection id ' . $connectionId);
+            $sessionKey = self::SESSION_KEY_MYSQL_CONNECTION_IDS . '_' . $request->path();
+            if ($session->has($sessionKey)) {
+                $oldConnectionIds = $session->pull($sessionKey);
+                foreach ($oldConnectionIds as $oldConnectionId) {
+                    try {
+                        Log::debug(getmypid() . ': Killing: ' . $oldConnectionId);
+                        $this->connection->statement('KILL ?', [$oldConnectionId]);
+                    } catch (QueryException $e) {
+                        Log::debug(getmypid() . ': ' . $e->getMessage());
+                        if (strpos(
+                            $e->getMessage(),
+                            'SQLSTATE[HY000]: General error: 1094 Unknown thread id:'
+                            ) !== 0) {
+                            throw $e;
+                        }
+                    }
+                }
+            }
+            $session->push($sessionKey, $connectionId);
+            $session->save();
+            Log::debug(getmypid() . ': connection id saved to session', [$connectionId]);
+
+            //dd($session);
         }
 
         return $next($request);
