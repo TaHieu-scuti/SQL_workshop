d77bb28ad6b1b62c3476fb479a97ae89118f75df
TaHieu-scuti <hieutq@scuti.asia>
2018-03-21 11:20:25 +0700

    undo unset clomun

--- app/Http/Controllers/AbstractReportController.php
+++ app/Http/Controllers/AbstractReportController.php
@@ -790,7 +790,7 @@ abstract class AbstractReportController extends Controller
     public function updateModelForPrefecture()
     {
         $fieldNames = session(static::SESSION_KEY_FIELD_NAME);
-        $fieldNames = $this->model->unsetColumns($fieldNames, ['impressionShare']);
+        $fieldNames = $this->model->unsetColumns($fieldNames, ['impressionShare', 'matchType']);
         session()->put([static::SESSION_KEY_FIELD_NAME => $fieldNames]);
 
         if (session(self::SESSION_KEY_ENGINE) === 'yss') {
