5b8fd1ba158703b736d7f33a8abb65115a9f9b02
lethu96 <lethucntt1@gmail.com>
2018-03-22 09:24:31 +0700

    fix some css problem

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -305,7 +305,7 @@
     <!-- Lib js -->
     <script type="text/javascript" src="/js/moment.min.js"></script>
     <script type="text/javascript" src="/js/jquery-1.11.3.min.js"></script>
-    <script type="text/javascript" src="/js/jquery.daterangepicker.min.js"></script>
+    <script type="text/javascript" src="/js/jquery.daterangepicker.js"></script>
     <script src="/js/bootstrap.min.js"></script>
     <script src="/js/jquery-ui.min.js"></script>
     <script src="/js/jquery-date-range.js"></script>
