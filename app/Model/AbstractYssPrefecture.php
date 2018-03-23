86b5319e61092952952eec512d1af0f4883aabb0
TaHieu-scuti <hieutq@scuti.asia>
2018-03-23 14:31:41 +0700

    refactor code of yss prefecture

--- app/Model/AbstractYssPrefecture.php
+++ app/Model/AbstractYssPrefecture.php
@@ -7,61 +7,6 @@ use Illuminate\Support\Facades\DB;
 
 abstract class AbstractYssPrefecture extends AbstractYssSpecificReportModel
 {
-    protected function updateTemporaryTableWithConversion(
-        $conversionPoints,
-        $groupedByField,
-        $startDay,
-        $endDay,
-        $engine,
-        $clientId = null,
-        $accountId = null,
-        $campaignId = null,
-        $adGroupId = null,
-        $adReportId = null,
-        $keywordId = null
-    ) {
-        $conversionNames = array_values(array_unique($conversionPoints->pluck('conversionName')->toArray()));
-        foreach ($conversionNames as $key => $conversionName) {
-            $convModel = new RepoYssPrefectureReportConv();
-            $queryGetConversion = $convModel->select(
-                DB::raw('SUM(repo_yss_prefecture_report_conv.conversions) AS conversions, '.$groupedByField)
-            )->where('conversionName', $conversionName)
-                ->where(
-                    function (EloquentBuilder $query) use (
-                        $convModel,
-                        $startDay,
-                        $endDay,
-                        $engine,
-                        $clientId,
-                        $accountId,
-                        $campaignId,
-                        $adGroupId,
-                        $adReportId,
-                        $keywordId
-                    ) {
-                        $convModel->getCondition(
-                            $query,
-                            $startDay,
-                            $endDay,
-                            $engine,
-                            $clientId,
-                            $accountId,
-                            $campaignId,
-                            $adGroupId,
-                            $adReportId,
-                            $keywordId
-                        );
-                    }
-                )->groupBy($groupedByField);
-
-            DB::update(
-                'update '.self::TABLE_TEMPORARY.', ('
-                .$this->getBindingSql($queryGetConversion).')AS tbl set conversions'.$key.' = tbl.conversions where '
-                .self::TABLE_TEMPORARY.'.'.$groupedByField.' = tbl.'.$groupedByField
-            );
-        }
-    }
-
     protected function updateTemporaryTableWithCallTracking(
         $adGainerCampaigns,
         $groupedByField,
--- app/Model/RepoYssAdgroupPrefecture.php
+++ app/Model/RepoYssAdgroupPrefecture.php
@@ -12,6 +12,63 @@ class RepoYssAdgroupPrefecture extends AbstractYssPrefecture
 
     public $timestamps = false;
 
+    protected function updateTemporaryTableWithConversion(
+        $conversionPoints,
+        $groupedByField,
+        $startDay,
+        $endDay,
+        $engine,
+        $clientId = null,
+        $accountId = null,
+        $campaignId = null,
+        $adGroupId = null,
+        $adReportId = null,
+        $keywordId = null
+    ) {
+        $conversionNames = array_values(array_unique($conversionPoints->pluck('conversionName')->toArray()));
+        $adgroupIDs = array_unique($conversionPoints->pluck('adgroupID')->toArray());
+        foreach ($conversionNames as $key => $conversionName) {
+            $convModel = new RepoYssPrefectureReportConv();
+            $queryGetConversion = $convModel->select(
+                DB::raw('SUM(repo_yss_prefecture_report_conv.conversions) AS conversions, '.$groupedByField)
+            )->where('conversionName', $conversionName)
+                ->where('adgroupID', $adgroupIDs)
+                ->where(
+                    function (EloquentBuilder $query) use (
+                        $convModel,
+                        $startDay,
+                        $endDay,
+                        $engine,
+                        $clientId,
+                        $accountId,
+                        $campaignId,
+                        $adGroupId,
+                        $adReportId,
+                        $keywordId
+                    ) {
+                        $convModel->getCondition(
+                            $query,
+                            $startDay,
+                            $endDay,
+                            $engine,
+                            $clientId,
+                            $accountId,
+                            $campaignId,
+                            $adGroupId,
+                            $adReportId,
+                            $keywordId
+                        );
+                    }
+                )->groupBy($groupedByField);
+
+            DB::update(
+                'update '.self::TABLE_TEMPORARY.', ('
+                .$this->getBindingSql($queryGetConversion).')AS tbl set conversions'.$key.' = tbl.conversions where '
+                .self::TABLE_TEMPORARY.'.'.$groupedByField.' = tbl.'.$groupedByField
+            );
+        }
+    }
+
     protected function getAllDistinctConversionNames($account_id, $accountId, $campaignId, $adGroupId, $column)
     {
         $yssPrefectureConvModel = new RepoYssPrefectureReportConv();
