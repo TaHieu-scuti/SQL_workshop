36715861d1f23f787639d9a4759b5c43112deb51
Duong Nguyen <duongnx@scuti.asia>
2018-03-27 15:28:17 +0700

    removed semicolon

--- app/Model/RepoAdwAdTimezone.php
+++ app/Model/RepoAdwAdTimezone.php
@@ -26,7 +26,7 @@ class RepoAdwAdTimezone extends AbstractAdwSubReportModel
         $sort,
         $groupedByField,
         $agencyId = null,
-        $accountId = null,';'
+        $accountId = null,
         $clientId = null,
         $campaignId = null,
         $adGroupId = null,
