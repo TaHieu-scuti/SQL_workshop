028748de80ae1b981645554e6d268b79cd068f99
TungNguyenLelele <tungxn1594@gmail.com>
2018-04-03 18:26:08 +0700

    work with redis only for now

--- app/Http/Controllers/AbstractReportController.php
+++ app/Http/Controllers/AbstractReportController.php
@@ -122,7 +122,7 @@ abstract class AbstractReportController extends Controller
     ) {
         $this->responseFactory = $responseFactory;
         $this->model = $model;
-        $this->middleware('auth');
+        $this->middleware('auth:redisGuard');
         $this->middleware('language');
         $this->middleware('killOldMySqlConnections');
     }
