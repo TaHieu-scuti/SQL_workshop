08ff57216bae4bc13554c91324fda48667a72a4c
tiennc <nguyenchitien195@gmail.com>
2018-04-05 15:46:20 +0700

    add condition to add blueline under type of report on account page

--- resources/views/yssAccountReport/index.blade.php
+++ resources/views/yssAccountReport/index.blade.php
@@ -1,3 +1,8 @@
+<?php
+use App\Http\Controllers\RepoYssAccountReport\RepoYssAccountReportController;
+
+$accountGrouped = session(RepoYssAccountReportController::SESSION_KEY_GROUPED_BY_FIELD);
+?>
 @extends('layouts.master')
 
 @section('title')
@@ -6,7 +11,7 @@
 
 @section('filter-list')
     <ul class="panel">
-        <li class="panel-body normal-report">
+        <li class="panel-body normal-report @if ($accountGrouped === 'accountName') active @endif">
             <a href="javascript:void(0)">
                 @lang('language.engineAccount')
             </a>
@@ -25,21 +30,25 @@
         </li>
         <li class="panel-body separator">
         </li>
-        <li class="panel-body specific-filter-item" data-value="prefecture">
+        <li class="panel-body specific-filter-item @if ($accountGrouped === 'prefecture') active @endif"
+        data-value="prefecture">
             <a href="javascript:void(0)">
                 @lang('language.PREFECTURES')
             </a>
         </li>
-        <li class="panel-body specific-filter-item" data-value="hourofday">
+        <li class="panel-body specific-filter-item @if ($accountGrouped === 'hourofday') active @endif"
+        data-value="hourofday">
             <a href="javascript:void(0)">
                 @lang('language.BY_TIME_ZONE')
 
-        <li class="panel-body specific-filter-item" data-value="dayOfWeek">
+        <li class="panel-body specific-filter-item @if ($accountGrouped === 'dayOfWeek') active @endif"
+        data-value="dayOfWeek">
             <a href="javascript:void(0)">
                 @lang('language.BY_DAYS_OF_THE_WEEK')
             </a>
         </li>
-        <li class="panel-body specific-filter-item" data-value="device">
+        <li class="panel-body specific-filter-item @if ($accountGrouped === 'device') active @endif"
+        data-value="device">
             <a href="javascript:void(0)">
                 @lang('language.DEVICES')
             </a>
