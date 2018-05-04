22e32f8befc3eae6a5f61aeab987adb3250956e5
Duong Nguyen <duongnx@scuti.asia>
2018-05-04 09:31:51 +0700

    retrieved conversion names

--- app/Model/RepoAdwSearchQueryPerformanceReport.php
+++ app/Model/RepoAdwSearchQueryPerformanceReport.php
@@ -2,13 +2,31 @@
 
 namespace App\Model;
 
-use App\AbstractReportModel;
+use App\Model\AbstractAdwModel;
+use App\Model\RepoAdwSearchQueryPerformanceReportConv;
 
-class RepoAdwSearchQueryPerformanceReport extends AbstractReportModel
+use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
+
+class RepoAdwSearchQueryPerformanceReport extends AbstractAdwModel
 {
+    const PAGE_ID = 'keywordID';
     protected $table = 'repo_adw_search_query_performance_report_cost';
 
     public $timestamps = false;
 
     protected $isSearchQueryReport = true;
+
+    public function getAllDistinctConversionNames($account_id, $accountId, $campaignId, $adGroupId, $column)
+    {
+        $adw_search_query_conv_model = new RepoAdwSearchQueryPerformanceReportConv();
+        $aggregation = $this->getAggregatedConversionName($column);
+        return $adw_search_query_conv_model->select($aggregation)
+            ->distinct()
+            ->where(
+                function (EloquentBuilder $query) use ($account_id, $accountId, $campaignId, $adGroupId) {
+                    $this->addConditonForConversionName($query, $account_id, $accountId, $campaignId, $adGroupId);
+                }
+            );
+            ->get();
+    }
 }
