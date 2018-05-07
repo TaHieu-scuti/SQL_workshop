62a53eb813739ea57d814989807f680de3ea4160
Duong Nguyen <duongnx@scuti.asia>
2018-05-07 17:15:18 +0700

    added updateTemporaryTableWithConversion()

--- app/Model/RepoAdwSearchQueryPerformanceReport.php
+++ app/Model/RepoAdwSearchQueryPerformanceReport.php
@@ -6,10 +6,13 @@ use App\Model\AbstractAdwModel;
 use App\Model\RepoAdwSearchQueryPerformanceReportConv;
 
 use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
+use Illuminate\Support\Facades\DB;
 
 class RepoAdwSearchQueryPerformanceReport extends AbstractAdwModel
 {
     const PAGE_ID = 'keywordID';
+    const GROUPED_BY_FIELD_NAME = 'keyword';
+
     protected $table = 'repo_adw_search_query_performance_report_cost';
 
     public $timestamps = false;
@@ -26,7 +29,65 @@ class RepoAdwSearchQueryPerformanceReport extends AbstractAdwModel
                 function (EloquentBuilder $query) use ($account_id, $accountId, $campaignId, $adGroupId) {
                     $this->addConditonForConversionName($query, $account_id, $accountId, $campaignId, $adGroupId);
                 }
-            );
+            )
             ->get();
+    }
+
+    protected function updateTemporaryTableWithConversion(
+        $conversionPoints,
+        $groupedByField,
+        $startDay,
+        $endDay,
+        $engine,
+        $clientId = null,
+        $accountId = null,
+        $campaignId = null,
+        $adGroupId = null,
+        $adReportId = null,
+        $keywordId = null
+    ) {
+        $conversionNames = array_values(array_unique($conversionPoints->pluck('conversionName')->toArray()));
+        foreach ($conversionNames as $key => $conversionName) {
+            $convModel = new RepoAdwSearchQueryPerformanceReportConv;
+            $queryGetConversion = $convModel->select(
+                array_merge(
+                    [DB::raw('SUM(repo_adw_search_query_performance_report_conv.conversions) AS conversions')],
+                    $this->groupBy
+                )
+            )->where('conversionName', $conversionName)
+                ->where(
+                    function (EloquentBuilder $query) use (
+                        $convModel,
+                        $startDay,
+                        $endDay,
+                        $engine,
+                        $clientId,
+                        $accountId,
+                        $campaignId,
+                        $adGroupId,
+                        $adReportId,
+                        $keywordId
+                    ) {
+                        $convModel->getCondition(
+                            $query,
+                            $startDay,
+                            $endDay,
+                            $engine,
+                            $clientId,
+                            $accountId,
+                            $campaignId,
+                            $adGroupId,
+                            $adReportId,
+                            $keywordId
+                        );
+                    }
+                )->groupBy($this->groupBy);
+            DB::update(
+                'update '.self::TABLE_TEMPORARY.', ('
+                .$this->getBindingSql($queryGetConversion).')AS tbl set conversions'.$key.' = tbl.conversions where '
+                .self::TABLE_TEMPORARY.'.'.$groupedByField.' = tbl.'.$groupedByField. ' AND '
+                .self::TABLE_TEMPORARY.'.adGroupID = tbl.adGroupID'
+            );
+        }
     }
 }
